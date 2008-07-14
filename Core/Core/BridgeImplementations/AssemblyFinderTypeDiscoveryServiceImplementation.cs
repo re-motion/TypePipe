fcb5253701784081c3c25524b85bb2cd1d194cfa
Fabian Schmied <fabian.schmied@rubicon.eu>
2008-07-14 09:50:49 +0000

    Moved ContextAwareTypeDiscoveryUtility to Core.Interfaces Added IAssemblyFinderTypeDiscoveryServiceImplementation as a bridge interface for ContextAwareTypeDiscoveryUtility

---
+++ Core/Core/BridgeImplementations/AssemblyFinderTypeDiscoveryServiceImplementation.cs
@@ -0,0 +1,25 @@
+/* Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the re:motion license agreement in license.txt. If you did not 
+ * receive it, please visit http://www.re-motion.org/licensing.
+ * 
+ * Unless otherwise provided, this software is distributed on an "AS IS" basis, 
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+ */
+
+using System;
+using System.ComponentModel.Design;
+using Remotion.BridgeInterfaces;
+using Remotion.Reflection;
+
+namespace Remotion.BridgeImplementations
+{
+  public class AssemblyFinderTypeDiscoveryServiceImplementation : IAssemblyFinderTypeDiscoveryServiceImplementation
+  {
+    public ITypeDiscoveryService CreateTypeDiscoveryService ()
+    {
+      return new AssemblyFinderTypeDiscoveryService (new AssemblyFinder (ApplicationAssemblyFinderFilter.Instance, false));
+    }
+  }
+}
