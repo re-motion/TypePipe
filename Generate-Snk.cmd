cc460e78d6ec1e7302de87c77982f835d40aa06a
Fabian Schmied <fabian.schmied@rubicon.eu>
2009-06-05 12:29:55 +0000

    Changed Generate-Snk.cmd to use exit codes Changed GenerateSetupDBScript.cmd to use 'exit /b' to avoid closing the active command shell

--- Generate-Snk.cmd
+++ Generate-Snk.cmd
@@ -19,6 +19,7 @@ sn.exe -k %keyfile%
 echo.
 
 if %ERRORLEVEL%==9009 goto nosn
+if not %ERRORLEVEL%==0 goto snerror
 
 echo Note that this newly generated key file will not match the original key used by rubicon to sign re-motion assemblies.
 
@@ -28,6 +29,14 @@ goto end
 
 echo 'sn.exe', which is a part of the .NET/Windows SDK, could not be found in the PATH. Please set the PATH to include the .NET/Windows SDK or run this script from a Visual Studio command prompt.
 
-goto end
+exit /b 1
+
+:snerror
+
+echo There was an error executing 'sn.exe'.
+
+exit /b 2
 
 :end
+
+exit /b 0
