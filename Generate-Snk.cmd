9cb6a8aaa19cd7ec6b76c67f38aa6d65f1e65cfa
Fabian Schmied <fabian.schmied@rubicon.eu>
2009-06-05 08:11:05 +0000

    Added script to generate remotion.snk if it doesn't exist (RM-1072)

---
+++ Generate-Snk.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+set keyfile=remotion.snk
+
+echo Current directory: %CD%
+echo Checking whether %keyfile% needs to be created...
+echo.
+if not exist %keyfile% goto notfound
+
+echo %keyfile% already exists.
+
+goto end
+
+:notfound
+
+echo %keyfile% does not exist, generating...
+echo.
+sn.exe -k %keyfile%
+echo.
+
+if errorlevel 9009 goto nosn
+
+echo Note that this newly generated key file will not match the original key used by rubicon to sign re-motion assemblies.
+
+goto end
+
+:nosn
+
+echo 'sn.exe', which is a part of the .NET/Windows SDK, could not be found in the PATH. Please set the PATH to include the .NET/Windows SDK or run this script from a Visual Studio command prompt.
+
+goto end
+
+:end
