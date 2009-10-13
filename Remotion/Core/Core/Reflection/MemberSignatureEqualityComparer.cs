a1d3ab10850fec0bd258ce1c4a9f325412b880d3
Fabian Schmied <fabian.schmied@rubicon.eu>
2009-10-13 09:50:06 +0000

    Added MemberSignatureEqualityComparer (RM-1621)

---
+++ Remotion/Core/Core/Reflection/MemberSignatureEqualityComparer.cs
@@ -0,0 +1,79 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Remotion.Reflection.SignatureStringBuilding;
+using Remotion.Utilities;
+
+namespace Remotion.Reflection
+{
+  /// <summary>
+  /// Compares the signatures of two members for equality. Two member signatures compare equal if and only if both their member types and
+  /// their signature strings (<see cref="N:Remotion.Reflection.SignatureStringBuilding"/>) are equal. This comparer does not support comparing
+  /// <see langword="null" /> values.
+  /// </summary>
+  public class MemberSignatureEqualityComparer : IEqualityComparer<MemberInfo>
+  {
+    private static readonly MethodSignatureStringBuilder s_methodSignatureBuilder = new MethodSignatureStringBuilder ();
+    private static readonly PropertySignatureStringBuilder s_propertySignatureBuilder = new PropertySignatureStringBuilder ();
+    private static readonly EventSignatureStringBuilder s_eventSignatureBuilder = new EventSignatureStringBuilder ();
+    
+    public bool Equals (MemberInfo x, MemberInfo y)
+    {
+      ArgumentUtility.CheckNotNull ("x", x);
+      ArgumentUtility.CheckNotNull ("y", y);
+
+      var signatureBuilderX = GetSignatureBuilder (x.MemberType);
+      var signatureBuilderY = GetSignatureBuilder (y.MemberType);
+
+      if (signatureBuilderX != signatureBuilderY)
+        return false;
+
+      var signatureX = signatureBuilderX.BuildSignatureString (x);
+      var signatureY = signatureBuilderY.BuildSignatureString (y);
+
+      return signatureX == signatureY;
+    }
+
+    public int GetHashCode (MemberInfo obj)
+    {
+      ArgumentUtility.CheckNotNull ("obj", obj);
+
+      var signatureBuilder = GetSignatureBuilder (obj.MemberType);
+      var signatureString = signatureBuilder.BuildSignatureString (obj);
+      return signatureString.GetHashCode ();
+    }
+
+    private IMemberSignatureStringBuilder GetSignatureBuilder (MemberTypes memberType)
+    {
+      switch (memberType)
+      {
+        case MemberTypes.Method:
+          return s_methodSignatureBuilder;
+        case MemberTypes.Property:
+          return s_propertySignatureBuilder;
+        case MemberTypes.Event:
+          return s_eventSignatureBuilder;
+        default:
+          var message = string.Format (
+              "MemberSignatureEqualityComparer does not support member type '{0}', only methods, properties, and events are supported.", 
+              memberType);
+          throw new NotSupportedException (message);
+      }
+    }
+  }
+}
---
+++ Remotion/Core/UnitTests/Reflection/MemberSignatureEqualityComparerTest.cs
@@ -0,0 +1,140 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Reflection;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Reflection;
+using Remotion.Reflection.SignatureStringBuilding;
+using Remotion.UnitTests.Reflection.TestDomain;
+
+namespace Remotion.UnitTests.Reflection
+{
+  [TestFixture]
+  public class MemberSignatureEqualityComparerTest
+  {
+    private MemberSignatureEqualityComparer _comparer;
+
+    private MethodInfo _m1;
+    private MethodInfo _m2;
+    private MethodInfo _m3;
+
+    private PropertyInfo _p1;
+    private PropertyInfo _p2;
+    private PropertyInfo _p3;
+
+    private EventInfo _e1;
+    private EventInfo _e2;
+    private EventInfo _e3;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _comparer = new MemberSignatureEqualityComparer ();
+
+      _m1 = typeof (ClassForSignatureComparisons).GetMethod ("M1");
+      _m2 = typeof (ClassForSignatureComparisons).GetMethod ("M2");
+      _m3 = typeof (ClassForSignatureComparisons).GetMethod ("M3");
+
+      _p1 = typeof (ClassForSignatureComparisons).GetProperty ("P1");
+      _p2 = typeof (ClassForSignatureComparisons).GetProperty ("P2");
+      _p3 = typeof (ClassForSignatureComparisons).GetProperty ("P3");
+
+      _e1 = typeof (ClassForSignatureComparisons).GetEvent ("E1");
+      _e2 = typeof (ClassForSignatureComparisons).GetEvent ("E2");
+      _e3 = typeof (ClassForSignatureComparisons).GetEvent ("E3");
+    }
+
+    [Test]
+    public void Equals_Methods_True ()
+    {
+      Assert.That (_comparer.Equals (_m1, _m2), Is.True);
+    }
+
+    [Test]
+    public void Equals_Methods_False ()
+    {
+      Assert.That (_comparer.Equals (_m1, _m3), Is.False);
+    }
+
+    [Test]
+    public void GetHashCode_Methods_Equal ()
+    {
+      Assert.That (_comparer.GetHashCode (_m1), Is.EqualTo (_comparer.GetHashCode (_m2)));
+    }
+
+    [Test]
+    public void Equals_Properties_True ()
+    {
+      Assert.That (_comparer.Equals (_p1, _p2), Is.True);
+    }
+
+    [Test]
+    public void Equals_Properties_False ()
+    {
+      Assert.That (_comparer.Equals (_p1, _p3), Is.False);
+    }
+
+    [Test]
+    public void GetHashCode_Properties_Equal ()
+    {
+      Assert.That (_comparer.GetHashCode (_p1), Is.EqualTo (_comparer.GetHashCode (_p2)));
+    }
+
+    [Test]
+    public void Equals_Events_True ()
+    {
+      Assert.That (_comparer.Equals (_e1, _e2), Is.True);
+    }
+
+    [Test]
+    public void Equals_Events_False ()
+    {
+      Assert.That (_comparer.Equals (_e1, _e3), Is.False);
+    }
+
+    [Test]
+    public void GetHashCode_Events_Equal ()
+    {
+      Assert.That (_comparer.GetHashCode (_e1), Is.EqualTo (_comparer.GetHashCode (_e2)));
+    }
+
+    [Test]
+    public void Equals_NonEqualMemberTypes ()
+    {
+      Assert.That (new PropertySignatureStringBuilder ().BuildSignatureString (_p1), 
+          Is.EqualTo (new MethodSignatureStringBuilder ().BuildSignatureString (_m1)));
+      
+      Assert.That (_comparer.Equals (_p1, _m1), Is.False);
+    }
+
+    [Test]
+    [ExpectedException (typeof (NotSupportedException),
+        ExpectedMessage = "MemberSignatureEqualityComparer does not support member type 'TypeInfo', only methods, properties, and events are supported.")]
+    public void Equals_InvalidMemberType ()
+    {
+      _comparer.Equals (_m1, typeof (object));
+    }
+
+    [Test]
+    [ExpectedException (typeof (NotSupportedException),
+        ExpectedMessage = "MemberSignatureEqualityComparer does not support member type 'TypeInfo', only methods, properties, and events are supported.")]
+    public void GetHashCode_InvalidMemberType ()
+    {
+      _comparer.GetHashCode (typeof (object));
+    }
+  }
+}
