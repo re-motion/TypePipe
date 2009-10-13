14a08760fe3c347ca4044498b9dce9d221934c66
Fabian Schmied <fabian.schmied@rubicon.eu>
2009-10-13 07:58:12 +0000

    Implemented MethodSignatureStringBuilder (RM-1621)

---
+++ Remotion/Core/Core/Reflection/MethodSignatureStringBuilder.cs
@@ -0,0 +1,112 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Remotion.Utilities;
+using System.Linq;
+
+namespace Remotion.Reflection
+{
+  /// <summary>
+  /// Builds a string representing the signature of a given <see cref="MethodInfo"/> object. This is similar to the string returned by 
+  /// <see cref="object.ToString"/> as it contains a textual representation of the method's return type, parameter types (without parameter names),
+  /// and generic parameter types. It's different from <see cref="object.ToString"/>, though, because it does not contain the method's name or 
+  /// generic parameter names.
+  /// </summary>
+  /// <remarks>
+  /// <para>
+  /// The signature string does currently not hold custom modifiers.
+  /// </para>
+  /// <para>
+  /// For simplicity, this class assumes that type or namespace names cannot contain the character "[". It encloses the parameter list with "(" 
+  /// and ")" and  separates the parameters by ",". It also assumes that the full name of a type (namespace, enclosing type (if any), and simple 
+  /// type name) is enough to identify a type - assembly information is not encoded. The 1:1 mapping of signature strings to method signatures is
+  /// only guaranteed for methods that adhere to these assumptions.
+  /// </para>
+  /// <para>
+  /// This class does not support closed generic methods, only generic method definitions are supported.
+  /// </para>
+  /// </remarks>
+  public class MethodSignatureStringBuilder
+  {
+    public string BuildSignatureString (MethodInfo methodInfo)
+    {
+      ArgumentUtility.CheckNotNull ("methodInfo", methodInfo);
+      if (methodInfo.IsGenericMethod && !methodInfo.IsGenericMethodDefinition)
+        throw new ArgumentException ("Closed generic methods are not supported.", "methodInfo");
+
+      var sb = new StringBuilder ();
+      
+      AppendTypeString (sb, methodInfo.ReturnType);
+      
+      sb.Append ("(");
+      AppendSeparatedTypeStrings (sb, methodInfo.GetParameters ().Select (p => p.ParameterType));
+      sb.Append (")");
+      
+      if (methodInfo.IsGenericMethod)
+        sb.Append ("`").Append (methodInfo.GetGenericArguments ().Length);
+
+      return sb.ToString ();
+    }
+
+    private void AppendTypeString (StringBuilder sb, Type type)
+    {
+      if (type.IsGenericParameter)
+      {
+        if (type.DeclaringMethod != null)
+        {
+          sb.Append ("[").Append (type.GenericParameterPosition).Append ("]");
+        }
+        else
+        {
+          sb.Append ("[").Append (type.GenericParameterPosition);
+          sb.Append ("/");
+          AppendTypeString (sb, type.DeclaringType);
+          sb.Append ("]");
+        }
+      }
+      else if (type.IsGenericTypeDefinition)
+      {
+        sb.Append (type.FullName); // Namespace.Type`Count
+      }
+      else if (type.IsGenericType)
+      {
+        AppendTypeString (sb, type.GetGenericTypeDefinition ());
+        sb.Append ("[");
+        AppendSeparatedTypeStrings (sb, type.GetGenericArguments ());
+        sb.Append ("]");
+      }
+      else
+      {
+        sb.Append (type.FullName);
+      }
+    }
+
+    private void AppendSeparatedTypeStrings (StringBuilder sb, IEnumerable<Type> types)
+    {
+      bool first = true;
+      foreach (var type in types)
+      {
+        if (!first)
+          sb.Append (",");
+        AppendTypeString (sb, type);
+        first = false;
+      }
+    }
+  }
+}
---
+++ Remotion/Core/UnitTests/Reflection/TestDomain/ClassForMethodSignatureStringBuilding.cs
@@ -0,0 +1,73 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+namespace Remotion.UnitTests.Reflection.TestDomain
+{
+  public class ClassForMethodSignatureStringBuilding<TType1, TType2>
+  {
+    public void MethodWithoutParameters ()
+    {
+    }
+
+    public void MethodWithParameters (string p1, DateTime p2)
+    {
+    }
+
+// ReSharper disable UnusedTypeParameter
+    public void MethodWithGenericParameters<T1, T2> (string p1, DateTime p2)
+// ReSharper restore UnusedTypeParameter
+    {
+    }
+
+    public T1 MethodWithUsedGenericParameters<T1, T2> (T2 p1)
+    {
+      return default (T1);
+    }
+
+    public TType1 MethodWithUsedGenericParametersFromOuterType (TType2 p1)
+    {
+      return default (TType1);
+    }
+
+    public void MethodWithClosedGenericType (ClassForMethodSignatureStringBuilding<int, string> p1)
+    {
+    }
+
+    public void MethodWithGenericTypeClosedWithGenericParameters<T1> (ClassForMethodSignatureStringBuilding<T1, TType1> p1)
+    {
+    }
+
+    public void MethodWithNestedType (Nested p1)
+    {
+    }
+
+    public void MethodWithNestedGenericType (NestedGeneric<int> p1)
+    {
+    }
+
+    public void MethodWithPartiallyClosedNestedGenericType<T1> (NestedGeneric<T1> p1, NestedGeneric<TType1> p2)
+    {
+    }
+
+    public class Nested
+    {
+    }
+
+    public class NestedGeneric<TNested>
+    {
+    }
+  }
+}
