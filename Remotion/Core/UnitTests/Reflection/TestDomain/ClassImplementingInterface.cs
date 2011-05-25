35ba83cc9de143377fbf77f3de302554bd1164a6
Fabian Schmied <fabian.schmied@rubicon.eu>
2011-05-25 09:35:28 +0000

    Moved test domain classes to unit test project

--- Remotion/Core/UnitTests/Reflection/TestDomain/ClassImplementingInterface.cs
+++ Remotion/Core/UnitTests/Reflection/TestDomain/ClassImplementingInterface.cs
@@ -14,9 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
-using System;
-
-namespace Remotion.Reflection.TestDomain
+namespace Remotion.UnitTests.Reflection.TestDomain
 {
   public class ClassImplementingInterface : IInterfaceToImplement
   {
