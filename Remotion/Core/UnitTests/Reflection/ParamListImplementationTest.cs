e6e9b8293ddaa95c5fe1a214a23f91c699d6d1c6
Michael Ketting <michael.ketting@rubicon.eu>
2011-11-10 13:57:39 +0000

    Updated license headers. (RM-4215)

--- Remotion/Core/UnitTests/Reflection/ParamListImplementationTest.cs
+++ Remotion/Core/UnitTests/Reflection/ParamListImplementationTest.cs
@@ -14,9 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
-// Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
-// All rights reserved.
-
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
