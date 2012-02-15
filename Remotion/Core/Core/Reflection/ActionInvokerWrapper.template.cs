440fba4d15e9e533954df24ee19ae3eacdce4f0a
Julian Lettner <julian.lettner@rubicon.eu>
2012-02-15 12:28:28 +0000

    Moved wrapper classes into their own source files (RM-4634)

--- Remotion/Core/Core/Reflection/ActionInvokerWrapper.template.cs
+++ Remotion/Core/Core/Reflection/ActionInvokerWrapper.template.cs
@@ -20,29 +20,39 @@ namespace Remotion.Reflection
 {
   // @begin-skip
   /// <summary>
-  /// This interface allows invokers with fixed arguments to be returned without references to their generic argument types. 
+  /// Used to wrap an <see cref="IActionInvoker"/> object rather than returning it directly.
   /// </summary>
-  /// <remarks>
-  /// <p>Note that casting a <see cref="T:Remotion.Reflection.ActionInvoker"/> to an interface is a boxing operation, thus creating an object on the
-  /// heap and garbage collecting it later. For very performance-critical scenarios, it be better to avoid this and accept the references to 
-  /// the invoker's generic argument types.</p>
-  /// <p>It is recommended to wrap this interface within a <see cref="ActionInvokerWrapper"/>, because returning an interface could lead to 
-  /// ambigous castings if the final call to <see cref="With{A1}"/> is missing, while using structs will usually lead to a compile-time error as 
-  /// expected.</p>
-  /// </remarks>
   // @end-skip
-  public partial interface IActionInvoker
+  public partial struct ActionInvokerWrapper : IActionInvoker
   {
     // @begin-skip
-    void Invoke (Type[] valueTypes, object[] values);
-    void Invoke (object[] values);
+    private readonly IActionInvoker _invoker;
+
+    public ActionInvokerWrapper (IActionInvoker invoker)
+    {
+      _invoker = invoker;
+    }
+
+    public void Invoke (Type[] valueTypes, object[] values)
+    {
+      _invoker.Invoke (valueTypes, values);
+    }
+
+    public void Invoke (object[] values)
+    {
+      _invoker.Invoke (values);
+    }
     // @end-skip
 
     // @begin-template first=1 generate=0..17 suppressTemplate=true
 
     // @replace "A<n>" ", " "<" ">"
     // @replace "A<n> a<n>" ", "
-    void With<A1> (A1 a1);
+    public void With<A1> (A1 a1)
+    {
+      // @replace "a<n>" ", "
+      _invoker.With (a1);
+    }
     // @end-template
   }
 }
--- Remotion/Core/Core/Reflection/FuncInvokerWrapper.template.cs
+++ Remotion/Core/Core/Reflection/FuncInvokerWrapper.template.cs
@@ -20,30 +20,56 @@ namespace Remotion.Reflection
 {
   // @begin-skip
   /// <summary>
-  /// This interface allows invokers with fixed arguments to be returned without references to their generic argument types. 
+  /// Used to wrap an <see cref="IFuncInvoker{TResult}"/> object rather than returning it directly.
   /// </summary>
-  /// <remarks>
-  /// <p>Note that casting a <see cref="T:Remotion.Reflection.FuncInvoker"/> struct to an interface is a boxing operation, thus creating an object on the
-  /// heap and garbage collecting it later. For very performance-critical scenarios, it be better to avoid this and accept the references to 
-  /// the invoker's generic argument types.</p>
-  /// <p>It is recommended to wrap this interface within a <see cref="FuncInvokerWrapper{TResult}"/>, because returning an interface could lead to 
-  /// ambigous castings if the final call to <see cref="With{A1}"/> is missing, while using structs will usually lead to a compile-time error as 
-  /// expected.</p>
-  /// </remarks>
   /// <typeparam name="TResult"> Return type of the method that will be invoked. </typeparam>
   // @end-skip
-  public partial interface IFuncInvoker<TResult>
+  public partial struct FuncInvokerWrapper<TResult> : IFuncInvoker<TResult>
   {
     // @begin-skip
-    TResult Invoke (Type[] valueTypes, object[] values);
-    TResult Invoke (object[] values);
+    private readonly IFuncInvoker<TResult> _invoker;
+    private readonly Func<TResult, TResult> _afterAction;
+
+    public FuncInvokerWrapper (IFuncInvoker<TResult> invoker)
+        : this (invoker, null)
+    {
+    }
+
+    public FuncInvokerWrapper (IFuncInvoker<TResult> invoker, Func<TResult, TResult> afterAction)
+    {
+      _invoker = invoker;
+      _afterAction = afterAction;
+    }
+
+    public TResult Invoke (Type[] valueTypes, object[] values)
+    {
+      return PerformAfterAction (_invoker.Invoke (valueTypes, values));      
+    }
+
+    public TResult Invoke (object[] values)
+    {
+      return PerformAfterAction (_invoker.Invoke (values));
+    }
+
+    private TResult PerformAfterAction (TResult result)
+    {
+      if (_afterAction != null)
+        result = _afterAction (result);
+      return result;
+    }
+
     // @end-skip
 
     // @begin-template first=1 generate=0..17 suppressTemplate=true
 
     // @replace "A<n>" ", " "<" ">"
     // @replace "A<n> a<n>" ", "
-    TResult With<A1> (A1 a1);
+    public TResult With<A1> (A1 a1)
+    {
+      // @replace "a<n>" ", "
+      return PerformAfterAction (_invoker.With (a1));
+    }
+
     // @end-template
   }
 }
