ee10d225599233f34234e30fa76165950c31ba9d
Fabian Schmied <fabian.schmied@rubicon.eu>
2012-02-23 14:44:36 +0000

    Fixed namespaces

--- Remotion/Core/UnitTests/BridgeInterfaces/IParamListCreateImplementationTest.cs
+++ Remotion/Core/UnitTests/BridgeInterfaces/IParamListCreateImplementationTest.cs
@@ -14,13 +14,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
-using System;
 using NUnit.Framework;
 using Remotion.BridgeImplementations;
 using Remotion.BridgeInterfaces;
 using Remotion.ServiceLocation;
 
-namespace Remotion.UnitTests.Interfaces.BridgeInterfaces
+namespace Remotion.UnitTests.BridgeInterfaces
 {
   [TestFixture]
   public class IParamListCreateImplementationTest
