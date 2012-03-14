757f818e4837a29f1171a7f9668b16986a48d726
Julian Lettner <julian.lettner@rubicon.eu>
2012-03-14 14:30:00 +0000

    Added TypeModificationHandler (RM-4672)

---
+++ Remotion/TypePipe/Core/CodeGeneration/ReflectionEmit/TypeModificationHandler.cs
@@ -0,0 +1,58 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using Remotion.TypePipe.CodeGeneration.ReflectionEmit.BuilderAbstractions;
+using Remotion.TypePipe.MutableReflection;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.CodeGeneration.ReflectionEmit
+{
+  /// <summary>
+  /// Implements <see cref="ITypeModificationHandler"/> by applying the modifications made to a <see cref="MutableType"/> to a subclass proxy.
+  /// </summary>
+  public class TypeModificationHandler : ITypeModificationHandler
+  {
+    private readonly ITypeBuilder _subclassProxyBuilder;
+
+    public TypeModificationHandler (ITypeBuilder subclassProxyBuilder)
+    {
+      _subclassProxyBuilder = ArgumentUtility.CheckNotNull ("subclassProxyBuilder", subclassProxyBuilder);
+    }
+
+    public ITypeBuilder SubclassProxyBuilder
+    {
+      get { return _subclassProxyBuilder; }
+    }
+
+    public void HandleAddedInterface (Type addedInterface)
+    {
+      ArgumentUtility.CheckNotNull ("addedInterface", addedInterface);
+      _subclassProxyBuilder.AddInterfaceImplementation (addedInterface);
+    }
+
+    public void HandleAddedField (FutureFieldInfo addedField)
+    {
+      ArgumentUtility.CheckNotNull ("addedField", addedField);
+      _subclassProxyBuilder.DefineField (addedField.Name, addedField.FieldType, addedField.Attributes);
+    }
+
+    public void HandleAddedConstructor (MutableConstructorInfo addedConstructor)
+    {
+      throw new NotImplementedException ("TODO 4686");
+    }
+  }
+}
---
+++ Remotion/TypePipe/UnitTests/CodeGeneration/ReflectionEmit/TypeModificationHandlerTest.cs
@@ -0,0 +1,59 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using NUnit.Framework;
+using Remotion.TypePipe.CodeGeneration.ReflectionEmit;
+using Remotion.TypePipe.CodeGeneration.ReflectionEmit.BuilderAbstractions;
+using Remotion.TypePipe.UnitTests.MutableReflection;
+using Rhino.Mocks;
+
+namespace Remotion.TypePipe.UnitTests.CodeGeneration.ReflectionEmit
+{
+  [TestFixture]
+  public class TypeModificationHandlerTest
+  {
+    private ITypeBuilder _subclassProxyBuilderMock;
+    private TypeModificationHandler _handler;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _subclassProxyBuilderMock = MockRepository.GenerateMock<ITypeBuilder>();
+      _handler = new TypeModificationHandler (_subclassProxyBuilderMock);
+    }
+
+    [Test]
+    public void HandleAddedInterface ()
+    {
+      var addedInterface = ReflectionObjectMother.GetSomeInterfaceType();
+
+      _handler.HandleAddedInterface (addedInterface);
+
+      _subclassProxyBuilderMock.AssertWasCalled (mock => mock.AddInterfaceImplementation (addedInterface));
+    }
+
+    [Test]
+    public void HandleAddedField ()
+    {
+      var addedField = FutureFieldInfoObjectMother.Create();
+
+      _handler.HandleAddedField (addedField);
+
+      _subclassProxyBuilderMock.AssertWasCalled (mock => mock.DefineField (addedField.Name, addedField.FieldType, addedField.Attributes));
+    }
+  }
+}
