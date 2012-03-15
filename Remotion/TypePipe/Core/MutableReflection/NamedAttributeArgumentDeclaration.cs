483d34fabec07ef52925c69b797173ab232bef51
Julian Lettner <julian.lettner@rubicon.eu>
2012-03-15 13:00:57 +0000

    Added NamedAttributeArgumentDeclaration (RM-4672)

---
+++ Remotion/TypePipe/Core/MutableReflection/NamedAttributeArgumentDeclaration.cs
@@ -0,0 +1,58 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System.Reflection;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.MutableReflection
+{
+  /// <summary>
+  /// Holds the member and value for a named attribute argument declaration.
+  /// </summary>
+  public class NamedAttributeArgumentDeclaration
+  {
+    private readonly MemberInfo _memberInfo;
+    private readonly object _value;
+
+    public NamedAttributeArgumentDeclaration (PropertyInfo propertyInfo, object value)
+    {
+      ArgumentUtility.CheckNotNull ("propertyInfo", propertyInfo);
+      ArgumentUtility.CheckNotNullAndType ("value", value, propertyInfo.PropertyType);
+
+      _memberInfo = propertyInfo;
+      _value = value;
+    }
+
+    public NamedAttributeArgumentDeclaration (FieldInfo fieldInfo, object value)
+    {
+      ArgumentUtility.CheckNotNull ("fieldInfo", fieldInfo);
+      ArgumentUtility.CheckNotNullAndType ("value", value, fieldInfo.FieldType);
+
+      _memberInfo = fieldInfo;
+      _value = value;
+    }
+
+    public MemberInfo MemberInfo
+    {
+      get { return _memberInfo; }
+    }
+
+    public object Value
+    {
+      get { return _value; }
+    }
+  }
+}
---
+++ Remotion/TypePipe/UnitTests/MutableReflection/NamedAttributeArgumentDeclarationTest.cs
@@ -0,0 +1,80 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using NUnit.Framework;
+using Remotion.TypePipe.MutableReflection;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.UnitTests.MutableReflection
+{
+  [TestFixture]
+  public class NamedAttributeArgumentDeclarationTest
+  {
+    [Test]
+    public void Initialization_Property ()
+    {
+      var propertyInfo = typeof (ClassWithMembers).GetProperty ("Property");
+      int value = 7;
+
+      var declaration = new NamedAttributeArgumentDeclaration (propertyInfo, value);
+
+      Assert.That (declaration.MemberInfo, Is.SameAs (propertyInfo));
+      Assert.That (declaration.Value, Is.EqualTo (value));
+    }
+
+    [Test]
+    [ExpectedException (typeof (ArgumentTypeException), ExpectedMessage =
+      "Argument value has type System.String when type System.ValueType was expected.\r\nParameter name: value")]
+    public void Initialization_Property_ValueNotAssignable ()
+    {
+      var propertyInfo = typeof (ClassWithMembers).GetProperty ("Property");
+      string value = "not assignable";
+
+      new NamedAttributeArgumentDeclaration (propertyInfo, value);
+    }
+
+    [Test]
+    public void Initialization_Field ()
+    {
+      var fieldInfo = typeof (ClassWithMembers).GetField ("Field");
+      int value = 7;
+
+      var declaration = new NamedAttributeArgumentDeclaration (fieldInfo, value);
+
+      Assert.That (declaration.MemberInfo, Is.SameAs (fieldInfo));
+      Assert.That (declaration.Value, Is.EqualTo (value));
+    }
+
+    [Test]
+    [ExpectedException (typeof (ArgumentTypeException), ExpectedMessage =
+      "Argument value has type System.String when type System.ValueType was expected.\r\nParameter name: value")]
+    public void Initialization_Field_ValueNotAssignable ()
+    {
+      var fieldInfo = typeof (ClassWithMembers).GetField ("Field");
+      string value = "not assignable";
+
+      new NamedAttributeArgumentDeclaration (fieldInfo, value);
+    }
+
+    private class ClassWithMembers
+    {
+      public ValueType Field = null;
+
+      public ValueType Property { get; set; }
+    }
+  }
+}
