2cda4d0297e46cccaee657d7712b4a2a894d486d
Julian Lettner <julian.lettner@rubicon.eu>
2012-03-20 13:48:17 +0000

    Added ThisExpression and infrastructure interfaces for custom TypePipe expressions (RM-4694)

---
+++ Remotion/TypePipe/Core/Expressions/ITypePipeExpression.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Scripting.Ast;
+
+namespace Remotion.TypePipe.Expressions
+{
+  /// <summary>
+  /// Identifies an infrastructure <see cref="Expression"/> defined by the TypePipe that can be handled using an 
+  /// <see cref="ITypePipeExpressionVisitor"/>.
+  /// </summary>
+  public interface ITypePipeExpression
+  {
+    void Accept (ITypePipeExpressionVisitor visitor);
+  }
+}
---
+++ Remotion/TypePipe/Core/Expressions/ITypePipeExpressionVisitor.cs
@@ -0,0 +1,10 @@
+﻿namespace Remotion.TypePipe.Expressions
+{
+  /// <summary>
+  /// Defines an interface for classes handling <see cref="ITypePipeExpression"/> instances.
+  /// </summary>
+  public interface ITypePipeExpressionVisitor
+  {
+    void VisitThisExpression (ThisExpression thisExpression);
+  }
+}
