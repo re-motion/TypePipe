8f3349faa00f25a6b42f3fa4c2281e9bf0d66907
Julian Lettner <julian.lettner@rubicon.eu>
2012-05-14 11:02:51 +0000

    Added ReflectionObjectMother.GetSomeOverridingMethod (RM-4839)

--- Remotion/TypePipe/UnitTests/MutableReflection/ReflectionObjectMother.cs
+++ Remotion/TypePipe/UnitTests/MutableReflection/ReflectionObjectMother.cs
@@ -38,6 +38,7 @@ namespace Remotion.TypePipe.UnitTests.MutableReflection
     private static readonly MethodInfo[] s_staticMethod = EnsureNoNulls (new[] { typeof (object).GetMethod ("ReferenceEquals"), typeof (double).GetMethod ("IsNaN") });
     private static readonly MethodInfo[] s_virtualMethods = EnsureNoNulls (new[] { typeof (object).GetMethod ("ToString"), typeof(object).GetMethod("GetHashCode") });
     private static readonly MethodInfo[] s_nonVirtualMethods = EnsureNoNulls (new[] { typeof (object).GetMethod ("ReferenceEquals"), typeof (string).GetMethod ("Concat", new[] { typeof (object) }) });
+    private static readonly MethodInfo[] s_overridingMethods = EnsureNoNulls (new[] { typeof (DomainType).GetMethod ("Override"), typeof (DomainType).GetMethod ("ToString") });
     private static readonly MethodInfo[] s_finalMethods = EnsureNoNulls (new[] { typeof(DomainType).GetMethod("FinalMethod") });
     private static readonly MethodInfo[] s_nonGenericMethods = EnsureNoNulls (new[] { typeof (object).GetMethod ("ToString"), typeof (string).GetMethod ("Concat", new[] { typeof (object) }) });
     private static readonly MethodInfo[] s_genericMethods = EnsureNoNulls (new[] { typeof (Enumerable).GetMethod ("Empty"), typeof (ReflectionObjectMother).GetMethod ("GetRandomElement", BindingFlags.NonPublic | BindingFlags.Static) });
@@ -98,6 +99,7 @@ namespace Remotion.TypePipe.UnitTests.MutableReflection
               .Concat(s_genericMethods)
               .Concat(s_virtualMethods)
               .Concat(s_nonVirtualMethods)
+              .Concat(s_overridingMethods)
               .Concat(s_finalMethods)
               .ToArray());
     }
@@ -127,6 +129,13 @@ namespace Remotion.TypePipe.UnitTests.MutableReflection
     {
       var method = GetRandomElement (s_nonVirtualMethods);
       Assertion.IsFalse (method.IsVirtual);
+      return method;
+    }
+
+    public static MethodInfo GetSomeOverridingMethod ()
+    {
+      var method = GetRandomElement (s_overridingMethods);
+      Assertion.IsTrue (method.GetBaseDefinition() != method);
       return method;
     }
 
@@ -189,11 +198,16 @@ namespace Remotion.TypePipe.UnitTests.MutableReflection
     private class DomainTypeBase
     {
       public virtual void FinalMethod () { }
+
+      public virtual void Override () { }
     }
 
     private class DomainType : DomainTypeBase
     {
       public sealed override void FinalMethod () { }
+
+      public override void Override () { }
+      public override string ToString () { return ""; }
     }
   }
 }
