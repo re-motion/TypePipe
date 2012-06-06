d95b84728e681ffe9688ef7f7921c2b10f66f13f
Julian Lettner <julian.lettner@rubicon.eu>
2012-06-06 13:30:46 +0000

    Renamed integration test (RM-4886)

--- Remotion/TypePipe/IntegrationTests/ReferenceMutableMembersTest.cs
+++ Remotion/TypePipe/IntegrationTests/ReferenceMutableMembersTest.cs
@@ -24,7 +24,7 @@ using Remotion.TypePipe.MutableReflection;
 namespace TypePipe.IntegrationTests
 {
   [TestFixture]
-  public class MutableMemberReferencesTest : TypeAssemblerIntegrationTestBase
+  public class ReferenceMutableMembersTest : TypeAssemblerIntegrationTestBase
   {
     [Test]
     public void MutableMembersCanBeUsedInBodies ()
