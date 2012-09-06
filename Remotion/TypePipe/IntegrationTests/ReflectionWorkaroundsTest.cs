b7e5320565990533b0f30ee6c881e5c2f13e3f5d
Julian Lettner <julian.lettner@rubicon.eu>
2012-09-06 13:13:00 +0000

    Added integration test (RM-4988)

---
+++ Remotion/TypePipe/IntegrationTests/ReflectionWorkaroundsTest.cs
@@ -0,0 +1,73 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using System.Reflection;
+using Microsoft.Scripting.Ast;
+using NUnit.Framework;
+
+namespace TypePipe.IntegrationTests
+{
+  [Ignore("TODO 4988")]
+  [TestFixture]
+  public class ReflectionWorkaroundsTest : TypeAssemblerIntegrationTestBase
+  {
+    // https://connect.microsoft.com/VisualStudio/feedback/details/757478/overriding-a-propertys-accessor-via-a-methodimpl-can-cause-the-property-to-disappear-from-reflection
+    [Test]
+    public void PreventDisappearanceOfPropertiesWhenModifying ()
+    {
+      ModifyOrOverrideProperty (typeof (DomainType));
+    }
+
+    [Test]
+    public void PreventDisappearanceOfPropertiesWhenOverriding ()
+    {
+      ModifyOrOverrideProperty (typeof (DomainTypeBase));
+    }
+
+    private void ModifyOrOverrideProperty (Type declaringType)
+    {
+      var bindingFlags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly;
+      var property = declaringType.GetProperty ("Property", bindingFlags);
+      Assert.That (property, Is.Not.Null);
+
+      var type = AssembleType<DomainType> (
+          mutableType =>
+          {
+            var mutableGetter = mutableType.GetOrAddMutableMethod (property.GetGetMethod());
+            mutableGetter.SetBody (ctx => Expression.Constant (""));
+          });
+
+      Assert.That (type.GetProperty ("Property", bindingFlags), Is.Not.Null);
+    }
+
+    public class DomainTypeBase
+    {
+      public virtual string Property
+      {
+        get { return ""; }
+      }
+    }
+
+    public class DomainType : DomainTypeBase
+    {
+      public new virtual string Property
+      {
+        get { return ""; }
+      }
+    }
+  }
+}
