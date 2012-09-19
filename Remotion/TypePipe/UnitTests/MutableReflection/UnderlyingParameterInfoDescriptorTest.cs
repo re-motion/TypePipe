b08ba9990bc9f3835774578ce9eac9e4becb7b94
Julian Lettner <julian.lettner@rubicon.eu>
2012-09-19 15:47:58 +0000

    Cleaned up code

--- Remotion/TypePipe/UnitTests/MutableReflection/UnderlyingParameterInfoDescriptorTest.cs
+++ Remotion/TypePipe/UnitTests/MutableReflection/UnderlyingParameterInfoDescriptorTest.cs
@@ -15,14 +15,12 @@
 // under the License.
 // 
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
 using Remotion.Development.UnitTesting.Reflection;
 using System.Linq;
 using Remotion.TypePipe.MutableReflection;
-using Remotion.FunctionalProgramming;
 
 namespace Remotion.TypePipe.UnitTests.MutableReflection
 {
