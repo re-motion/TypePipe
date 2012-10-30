7a52d19d4908eb3ddd024b7ec973ca21977ff1e0
Julian Lettner <julian.lettner@rubicon.eu>
2012-10-30 09:06:40 +0000

    Added TypeExtensions (RM-5095)

---
+++ Remotion/TypePipe/Core/MutableReflection/TypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+
+using System;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.MutableReflection
+{
+  /// <summary>
+  /// <see cref="Type"/> extension methods that are useful when working with <see cref="MutableType"/>s.
+  /// </summary>
+  public static class TypeExtensions
+  {
+    public static bool IsRuntimeType (this Type type)
+    {
+      ArgumentUtility.CheckNotNull ("type", type);
+
+      return type.Namespace == "System" && type.Name == "RuntimeType";
+    }
+  }
+}
