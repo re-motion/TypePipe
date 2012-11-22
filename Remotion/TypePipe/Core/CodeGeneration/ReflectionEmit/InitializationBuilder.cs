ac24935ad5ed3142d9101a4e629747fb7d20f85c
Julian Lettner <julian.lettner@rubicon.eu>
2012-11-22 16:11:31 +0000

    Added IInitializationBuilder (RM-5199)

---
+++ Remotion/TypePipe/Core/CodeGeneration/ReflectionEmit/InitializationBuilder.cs
@@ -0,0 +1,97 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using System.Reflection;
+using Microsoft.Scripting.Ast;
+using Remotion.Collections;
+using Remotion.TypePipe.Caching;
+using Remotion.TypePipe.MutableReflection;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.CodeGeneration.ReflectionEmit
+{
+  /// <summary>
+  /// Implements <see cref="IInitializationBuilder"/>.
+  /// </summary>
+  /// <remarks>
+  /// This class is a implementation detail of <see cref="ISubclassProxyBuilder"/>.
+  /// </remarks>
+  public class InitializationBuilder : IInitializationBuilder
+  {
+    public MutableConstructorInfo HandleTypeInitializations (MutableType mutableType)
+    {
+      ArgumentUtility.CheckNotNull ("mutableType", mutableType);
+
+      if (mutableType.TypeInitializations.Count == 0)
+        return null;
+
+      var attributes = MethodAttributes.Private | MethodAttributes.Static;
+      var body = Expression.Block (typeof (void), mutableType.TypeInitializations);
+      var descriptor = ConstructorDescriptor.Create (attributes, ParameterDescriptor.EmptyParameters, body);
+      var typeInitializer = new MutableConstructorInfo (mutableType, descriptor);
+
+      return typeInitializer;
+    }
+
+    public Tuple<FieldInfo, MethodInfo> HandleInstanceInitializations (MutableType mutableType)
+    {
+      ArgumentUtility.CheckNotNull ("mutableType", mutableType);
+
+      if (mutableType.InstanceInitializations.Count == 0)
+        return null;
+
+      mutableType.AddInterface (typeof (IInitializableObject));
+
+      var counter = mutableType.AddField ("_<TypePipe-generated>_ctorRunCounter", typeof (int), FieldAttributes.Private);
+
+      var interfaceMethod = MemberInfoFromExpressionUtility.GetMethod ((IInitializableObject obj) => obj.Initialize ());
+      var name = MethodOverrideUtility.GetNameForExplicitOverride (interfaceMethod);
+      var attributes = MethodOverrideUtility.GetAttributesForExplicitOverride (interfaceMethod).Unset (MethodAttributes.Abstract);
+      var parameters = ParameterDeclaration.CreateForEquivalentSignature (interfaceMethod);
+      var body = Expression.Block (interfaceMethod.ReturnType, mutableType.InstanceInitializations);
+      var initMethod = mutableType.AddMethod (name, attributes, interfaceMethod.ReturnType, parameters, ctx => body);
+      initMethod.AddExplicitBaseDefinition (interfaceMethod);
+
+      return Tuple.Create<FieldInfo, MethodInfo> (counter, initMethod);
+    }
+
+    public void WireConstructorWithInitialization (MutableConstructorInfo constructor, Tuple<FieldInfo, MethodInfo> initializationMembers)
+    {
+      ArgumentUtility.CheckNotNull ("constructor", constructor);
+
+      if (initializationMembers == null)
+        return;
+
+      // We cannot protect the decrement with try-finally because the this pointer must be initialized before entering a try block.
+      // Using *IncrementAssign and *DecrementAssign results in un-verifiable code (emits stloc.0).
+      constructor.SetBody (
+          ctx =>
+          {
+            var counter = Expression.Field (ctx.This, initializationMembers.Item1);
+            var one = Expression.Constant (1);
+
+            return Expression.Block (
+                Expression.Assign (counter, Expression.Add (counter, one)),
+                constructor.Body,
+                Expression.Assign (counter, Expression.Subtract (counter, one)),
+                Expression.IfThen (
+                    Expression.Equal (counter, Expression.Constant (0)),
+                    Expression.Call (ctx.This, initializationMembers.Item2)));
+          });
+    }
+  }
+}
---
+++ Remotion/TypePipe/UnitTests/CodeGeneration/ReflectionEmit/InitializationBuilderTest.cs
@@ -0,0 +1,160 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Scripting.Ast;
+using NUnit.Framework;
+using Remotion.Collections;
+using Remotion.Development.UnitTesting.Reflection;
+using Remotion.TypePipe.Caching;
+using Remotion.TypePipe.CodeGeneration.ReflectionEmit;
+using Remotion.TypePipe.Expressions;
+using Remotion.TypePipe.MutableReflection;
+using Remotion.TypePipe.UnitTests.Expressions;
+using Remotion.TypePipe.UnitTests.MutableReflection;
+using Rhino.Mocks;
+
+namespace Remotion.TypePipe.UnitTests.CodeGeneration.ReflectionEmit
+{
+  [TestFixture]
+  public class InitializationBuilderTest
+  {
+    private InitializationBuilder _builder;
+
+    private MutableType _mutableType;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _builder = new InitializationBuilder();
+
+      _mutableType = MutableTypeObjectMother.Create();
+    }
+
+    [Test]
+    public void HandleTypeInitializations ()
+    {
+      var expression = ExpressionTreeObjectMother.GetSomeExpression();
+      _mutableType.AddTypeInitialization (ctx => expression);
+
+      var result = _builder.HandleTypeInitializations (_mutableType);
+
+      Assert.That (result.DeclaringType, Is.SameAs (_mutableType));
+      Assert.That (result.Name, Is.EqualTo (".cctor"));
+      Assert.That (result.Attributes, Is.EqualTo (MethodAttributes.Private | MethodAttributes.Static));
+      Assert.That (result.GetParameters(), Is.Empty);
+      Assert.That (result.Body, Is.InstanceOf<BlockExpression>());
+
+      var blockExpression = (BlockExpression) result.Body;
+      Assert.That (blockExpression.Type, Is.EqualTo (typeof (void)));
+      Assert.That (blockExpression.Expressions, Is.EqualTo (new[] { expression }));
+    }
+
+    [Test]
+    public void HandleTypeInitializations_Empty ()
+    {
+      var result = _builder.HandleTypeInitializations (_mutableType);
+
+      Assert.That (result, Is.Null);
+    }
+
+    [Test]
+    public void HandleInstanceInitializations ()
+    {
+      var initExpression = ExpressionTreeObjectMother.GetSomeExpression ();
+      _mutableType.AddInstanceInitialization (ctx => initExpression);
+
+      var result = _builder.HandleInstanceInitializations (_mutableType);
+
+      var counter = result.Item1;
+      var initMethod = (MutableMethodInfo) result.Item2;
+
+      // Interface added.
+      Assert.That (_mutableType.AddedInterfaces, Is.EqualTo (new[] { typeof (IInitializableObject) }));
+
+      // Field added.
+      Assert.That (_mutableType.AddedFields, Is.EqualTo (new[] { counter }));
+      Assert.That (counter.Name, Is.EqualTo ("_<TypePipe-generated>_ctorRunCounter"));
+      Assert.That (counter.FieldType, Is.SameAs (typeof (int)));
+
+      // Initialization method added.
+      var methodAttributes = MethodAttributes.Private | MethodAttributes.Virtual | MethodAttributes.NewSlot | MethodAttributes.HideBySig;
+      Assert.That (_mutableType.AddedMethods, Is.EqualTo (new[] { initMethod }));
+      Assert.That (initMethod.DeclaringType, Is.SameAs (_mutableType));
+      Assert.That (initMethod.Name, Is.EqualTo ("Remotion.TypePipe.Caching.IInitializableObject_Initialize"));
+      Assert.That (initMethod.Attributes, Is.EqualTo (methodAttributes));
+      Assert.That (initMethod.ReturnType, Is.SameAs (typeof (void)));
+      Assert.That (initMethod.GetParameters (), Is.Empty);
+      Assert.That (initMethod.Body.Type, Is.SameAs (typeof (void)));
+      Assert.That (initMethod.Body, Is.InstanceOf<BlockExpression> ());
+      var blockExpression = (BlockExpression) initMethod.Body;
+      Assert.That (blockExpression.Expressions, Is.EqualTo (new[] { initExpression }));
+      var interfaceMethod = NormalizingMemberInfoFromExpressionUtility.GetMethod ((IInitializableObject obj) => obj.Initialize ());
+      Assert.That (initMethod.AddedExplicitBaseDefinitions, Is.EqualTo (new[] { interfaceMethod }));
+    }
+
+    [Test]
+    public void HandleInstanceInitializations_Empty ()
+    {
+      var result = _builder.HandleInstanceInitializations (_mutableType);
+
+      Assert.That (result, Is.Null);
+      Assert.That (_mutableType.AddedInterfaces, Is.Empty);
+      Assert.That (_mutableType.AddedFields, Is.Empty);
+      Assert.That (_mutableType.AddedMethods, Is.Empty);
+    }
+
+    [Test]
+    public void WireConstructorWithInitialization ()
+    {
+      var counter = MutableFieldInfoObjectMother.Create (_mutableType, type: typeof (int));
+      var initMethod = MutableMethodInfoObjectMother.Create (_mutableType);
+      var initializationMembers = Tuple.Create<FieldInfo, MethodInfo> (counter, initMethod);
+
+      var constructor = MutableConstructorInfoObjectMother.Create();
+      var oldBody = constructor.Body;
+
+      _builder.WireConstructorWithInitialization (constructor, initializationMembers);
+
+      var expectedBody = Expression.Block (
+          Expression.Assign (
+              Expression.Field (new ThisExpression (_mutableType), counter),
+              Expression.Add (Expression.Field (new ThisExpression (_mutableType), counter), Expression.Constant (1))),
+          oldBody,
+          Expression.Assign (
+              Expression.Field (new ThisExpression (_mutableType), counter),
+              Expression.Subtract (Expression.Field (new ThisExpression (_mutableType), counter), Expression.Constant (1))),
+          Expression.IfThen (
+              Expression.Equal (Expression.Field (new ThisExpression (_mutableType), counter), Expression.Constant (0)),
+              Expression.Call (new ThisExpression (_mutableType), initMethod)));
+
+      ExpressionTreeComparer.CheckAreEqualTrees (expectedBody, constructor.Body);
+    }
+
+    [Test]
+    public void WireConstructorWithInitialization_Null ()
+    {
+      var constructor = MutableConstructorInfoObjectMother.Create();
+      var oldBody = constructor.Body;
+
+      _builder.WireConstructorWithInitialization (constructor, initializationMembers: null);
+
+      Assert.That (constructor.Body, Is.SameAs (oldBody));
+    }
+  }
+}
