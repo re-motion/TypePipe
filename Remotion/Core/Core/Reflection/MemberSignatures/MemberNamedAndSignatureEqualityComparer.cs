d3a94890a396200af2bb799f4362a855cd54c052
Julian Lettner <julian.lettner@rubicon.eu>
2012-11-28 10:53:59 +0000

    Added MemberNamedAndSignatureEqualityComparer (RM-5227)

---
+++ Remotion/Core/Core/Reflection/MemberSignatures/MemberNamedAndSignatureEqualityComparer.cs
@@ -0,0 +1,47 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Remotion.Utilities;
+
+namespace Remotion.Reflection.MemberSignatures
+{
+  /// <summary>
+  /// Compares two members for equality by considering their name and signature.
+  /// This comparer does not support comparing <see langword="null" /> values.
+  /// </summary>
+  public class MemberNamedAndSignatureEqualityComparer : IEqualityComparer<MemberInfo>
+  {
+    private readonly MemberSignatureEqualityComparer _signatureComparer = new MemberSignatureEqualityComparer();
+
+    public bool Equals (MemberInfo x, MemberInfo y)
+    {
+      ArgumentUtility.CheckNotNull ("x", x);
+      ArgumentUtility.CheckNotNull ("y", y);
+
+      return x.Name == y.Name && _signatureComparer.Equals (x, y);
+    }
+
+    public int GetHashCode (MemberInfo obj)
+    {
+      ArgumentUtility.CheckNotNull ("obj", obj);
+
+      return EqualityUtility.GetRotatedHashCode (obj.Name, _signatureComparer.GetHashCode (obj));
+    }
+  }
+}
---
+++ Remotion/Core/UnitTests/Reflection/MemberSignatures/MemberNamedAndSignatureEqualityComparerTest.cs
@@ -0,0 +1,70 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Reflection;
+using NUnit.Framework;
+using Remotion.Development.UnitTesting;
+using Remotion.Development.UnitTesting.Reflection;
+using Remotion.Reflection.MemberSignatures;
+
+namespace Remotion.UnitTests.Reflection.MemberSignatures
+{
+  [TestFixture]
+  public class MemberNamedAndSignatureEqualityComparerTest
+  {
+    private MemberNamedAndSignatureEqualityComparer _comparer;
+
+    private MemberInfo _member;
+    private MemberInfo _memberDifferentSignature;
+    private MemberInfo _memberDifferentName;
+    private MemberInfo _memberSameNameAndSignature;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _comparer = new MemberNamedAndSignatureEqualityComparer();
+
+      _member = NormalizingMemberInfoFromExpressionUtility.GetMember (() => Member (""));
+      _memberDifferentSignature = NormalizingMemberInfoFromExpressionUtility.GetMember (() => Member (7));
+      _memberDifferentName = NormalizingMemberInfoFromExpressionUtility.GetMember (() => OtherMember (""));
+      _memberSameNameAndSignature = NormalizingMemberInfoFromExpressionUtility.GetMember ((ISomeInterface obj) => obj.Member (""));
+    }
+
+    [Test]
+    public void Equals ()
+    {
+      Assert.That (_comparer.Equals (_member, _memberDifferentSignature), Is.False);
+      Assert.That (_comparer.Equals (_member, _memberDifferentName), Is.False);
+      Assert.That (_comparer.Equals (_member, _memberSameNameAndSignature), Is.True);
+    }
+
+    [Test]
+    public new void GetHashCode ()
+    {
+      Assert.That (_comparer.GetHashCode (_member), Is.EqualTo (_comparer.GetHashCode (_memberSameNameAndSignature)));
+    }
+
+    void Member (string s) { Dev.Null = s; }
+    void Member (int l) { Dev.Null = l; }
+    void OtherMember (string s) { Dev.Null = s; }
+
+    interface ISomeInterface
+    {
+      void Member (string s);
+    }
+  }
+}
