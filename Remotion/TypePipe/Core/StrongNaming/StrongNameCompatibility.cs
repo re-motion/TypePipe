d3881f796a60a91809a9090e6c636e9eb39db85b
Julian Lettner <julian.lettner@rubicon.eu>
2012-12-21 08:47:23 +0000

    Documented StrongNameCompatibility. (RM-5292)

--- Remotion/TypePipe/Core/StrongNaming/StrongNameCompatibility.cs
+++ Remotion/TypePipe/Core/StrongNaming/StrongNameCompatibility.cs
@@ -20,13 +20,24 @@ using System;
 namespace Remotion.TypePipe.StrongNaming
 {
   /// <summary>
-  /// Specifies the compatibility of performed code modifications with strong-naming.
+  /// Specifies the compatibility of requested type modifications by a <see cref="IParticipant"/> with strong-naming.
   /// </summary>
-  // TODO 5292: Document impliciations (unknown --> poorer performance)
   public enum StrongNameCompatibility
   {
+    /// <summary>
+    /// The requested modifications are compatible with strong-naming, i.e., only types from strong-named assemblies are used for modifications.
+    /// </summary>
     Compatible,
+
+    /// <summary>
+    /// The requested modifications are not compatible with strong-naming, i.e., modifications include types from unsigned assemblies.
+    /// </summary>
     Incompatible,
+
+    /// <summary>
+    /// The participant is unsure if the requested modifications are strong-name compatible.
+    /// Note that this forces the pipeline to check strong-name compatibility which results in poorer performance if strong-naming is enabled.
+    /// </summary>
     Unknown
   }
 }
