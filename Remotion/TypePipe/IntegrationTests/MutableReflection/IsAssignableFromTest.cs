5a10b4e091127d6fb822cdbd4d7c117e7ea45965
Julian Lettner <julian.lettner@rubicon.eu>
2013-01-22 11:04:39 +0000

    Added failing integration test. (RM-5361)

---
+++ Remotion/TypePipe/IntegrationTests/MutableReflection/IsAssignableFromTest.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+
+using System;
+using NUnit.Framework;
+using Remotion.TypePipe.MutableReflection;
+
+namespace Remotion.TypePipe.IntegrationTests.MutableReflection
+{
+  [Ignore("TODO 5363")]
+  [TestFixture]
+  public class IsAssignableFromTest
+  {
+    private ProxyType _proxyType;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _proxyType = ProxyTypeObjectMother.Create (typeof (DomainType));
+    }
+
+    [Test]
+    public void UnderlyingSystemType ()
+    {
+      var result = _proxyType.UnderlyingSystemType;
+
+      Assert.That (result.IsRuntimeType(), Is.True);
+      Assert.That (_proxyType.UnderlyingSystemType, Is.SameAs (result));
+
+      _proxyType.AddInterface (typeof (IAddedInterface));
+      Assert.That (_proxyType.UnderlyingSystemType, Is.Not.SameAs (result));
+    }
+
+    [Test]
+    public void IsAssignableFrom ()
+    {
+      Assert.That (typeof (object).IsAssignableFrom (_proxyType), Is.True);
+      Assert.That (typeof (DomainType).IsAssignableFrom (_proxyType), Is.True);
+      Assert.That (_proxyType.IsAssignableFrom (_proxyType), Is.True);
+      Assert.That (_proxyType.IsAssignableFrom (typeof (DomainType)), Is.False);
+      Assert.That (typeof (IDomainInterface).IsAssignableFrom (_proxyType), Is.True);
+      Assert.That (typeof (IAddedInterface).IsAssignableFrom (_proxyType), Is.False);
+      Assert.That (typeof (UnrelatedType).IsAssignableFrom (_proxyType), Is.False);
+
+      _proxyType.AddInterface (typeof (IAddedInterface));
+      Assert.That (typeof (IAddedInterface).IsAssignableFrom (_proxyType), Is.True);
+    }
+
+    [Test]
+    public void IsSubclassOf ()
+    {
+      Assert.That (_proxyType.IsSubclassOf (typeof (object)), Is.True);
+      Assert.That (_proxyType.IsSubclassOf (typeof (DomainType)), Is.True);
+      Assert.That (_proxyType.IsSubclassOf (_proxyType), Is.False);
+      Assert.That (typeof (DomainType).IsSubclassOf (_proxyType), Is.False);
+      Assert.That (_proxyType.IsSubclassOf (typeof (IDomainInterface)), Is.False);
+    }
+
+    [Test]
+    public void Equals_Type ()
+    {
+      var proxyType = ProxyTypeObjectMother.Create (typeof (DomainType));
+
+      Assert.That (_proxyType.Equals (_proxyType), Is.True);
+      Assert.That (_proxyType.Equals (proxyType), Is.False);
+      // ReSharper disable CheckForReferenceEqualityInstead.1
+      Assert.That (_proxyType.Equals (typeof (DomainType)), Is.False);
+      // ReSharper restore CheckForReferenceEqualityInstead.1
+      Assert.That (typeof (DomainType).Equals (_proxyType), Is.False);
+    }
+
+    [Test]
+    public void Equals_Object ()
+    {
+      var proxyType = ProxyTypeObjectMother.Create (typeof (DomainType));
+
+      Assert.That (_proxyType.Equals ((object) _proxyType), Is.True);
+      Assert.That (_proxyType.Equals ((object) proxyType), Is.False);
+      Assert.That (_proxyType.Equals ((object) typeof (DomainType)), Is.False);
+      Assert.That (typeof (DomainType).Equals ((object) _proxyType), Is.False);
+    }
+
+    class DomainType : IDomainInterface { }
+    interface IDomainInterface { }
+    interface IAddedInterface { }
+    class UnrelatedType { }
+  }
+}
