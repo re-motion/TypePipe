3a57d21337ff9b64843ff002d94c890f8cd5087f
Julian Lettner <julian.lettner@rubicon.eu>
2013-01-31 10:33:52 +0000

    Updated exception message to make it more readable. (RM-5383)

--- Remotion/TypePipe/UnitTests/TypeAssembly/TypeAssemblerTest.cs
+++ Remotion/TypePipe/UnitTests/TypeAssembly/TypeAssemblerTest.cs
@@ -22,7 +22,6 @@ using Remotion.TypePipe.Caching;
 using Remotion.TypePipe.CodeGeneration;
 using Remotion.TypePipe.MutableReflection;
 using Remotion.TypePipe.MutableReflection.Implementation;
-using Remotion.TypePipe.Serialization;
 using Remotion.TypePipe.UnitTests.MutableReflection;
 using Rhino.Mocks;
 
@@ -114,7 +113,7 @@ namespace Remotion.TypePipe.UnitTests.TypeAssembly
       _subclassProxyCreatorMock.Expect (mock => mock.CreateProxy (Arg<ProxyType>.Is.Anything)).Throw (exception3);
       var typeAssembler = CreateTypeAssembler (participants: MockRepository.GenerateStub<IParticipant>());
 
-      var expectedMessageRegex = "An error occurred during code generation for 'ProxyName': blub "
+      var expectedMessageRegex = "An error occurred during code generation for 'ProxyName':\r\nblub\r\n"
                                  + @"The following participants are currently configured and may have caused the error: 'IParticipantProxy.*'\.";
       Assert.That (
           () => typeAssembler.AssembleType (_requestedType),
