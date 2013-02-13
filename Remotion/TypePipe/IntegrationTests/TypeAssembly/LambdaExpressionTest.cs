92279558fce6dc27d167303ae994da08552691f0
Julian Lettner <julian.lettner@rubicon.eu>
2013-02-13 10:50:41 +0000

    Removed refactoring leftover.

--- Remotion/TypePipe/IntegrationTests/TypeAssembly/LambdaExpressionTest.cs
+++ Remotion/TypePipe/IntegrationTests/TypeAssembly/LambdaExpressionTest.cs
@@ -34,8 +34,6 @@ namespace Remotion.TypePipe.IntegrationTests.TypeAssembly
     {
       base.SetUp ();
 
-      //AllowUnderlyingSystemTypeAccess();
-
       _invokeLambda = NormalizingMemberInfoFromExpressionUtility.GetMethod ((DomainType obj) => obj.InvokeLambda (7));
       _returnLambda = NormalizingMemberInfoFromExpressionUtility.GetMethod ((DomainType obj) => obj.ReturnLambda (7));
     }
