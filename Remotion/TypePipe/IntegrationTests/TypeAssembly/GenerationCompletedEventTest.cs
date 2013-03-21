6500f25a75d2c1943467f6cfec5001dd047504d9
Julian Lettner <julian.lettner@rubicon.eu>
2013-03-21 08:10:00 +0000

    Refined integration tests. (RM-5482)

--- Remotion/TypePipe/IntegrationTests/TypeAssembly/GenerationCompletedEventTest.cs
+++ Remotion/TypePipe/IntegrationTests/TypeAssembly/GenerationCompletedEventTest.cs
@@ -16,6 +16,7 @@
 // 
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.Scripting.Ast;
@@ -31,34 +32,36 @@ namespace Remotion.TypePipe.IntegrationTests.TypeAssembly
     [Test]
     public void GetGeneratedMember_Types ()
     {
-      Type generatedProxyType = null;
+      Type proxyType = null;
+      Type additionalType = null;
 
       var type = AssembleType<DomainType> (
           typeContext =>
           {
-            var additionalType = typeContext.CreateType ("MyType", "MyNamespace", TypeAttributes.Public, typeof (object));
+            var addedType = typeContext.CreateType ("MyType", "MyNamespace", TypeAttributes.Public, typeof (List<int>));
 
             typeContext.GenerationCompleted += ctx =>
             {
-              generatedProxyType = (Type) ctx.GetGeneratedMember (typeContext.ProxyType);
+              proxyType = (Type) ctx.GetGeneratedMember (typeContext.ProxyType);
+              additionalType = (Type) ctx.GetGeneratedMember (addedType);
 
-              var generatedAdditionalType = (Type) ctx.GetGeneratedMember (additionalType);
-              Assert.That (generatedAdditionalType.Name, Is.EqualTo ("MyType"));
-              Assert.That (generatedAdditionalType.Namespace, Is.EqualTo ("MyNamespace"));
-              Assert.That (generatedAdditionalType.Attributes, Is.EqualTo (TypeAttributes.Public));
-              Assert.That (generatedAdditionalType.BaseType, Is.SameAs (typeof (object)));
+              Assert.That (proxyType.IsRuntimeType(), Is.True);
+              Assert.That (additionalType.IsRuntimeType(), Is.True);
             };
           });
+      var expectedAdditionalType = type.Assembly.GetType ("MyNamespace.MyType", true);
 
-      Assert.That (type, Is.SameAs (generatedProxyType));
+      Assert.That (proxyType, Is.SameAs (type));
+      Assert.That (additionalType, Is.SameAs (expectedAdditionalType));
     }
 
     [Ignore ("TODO 5482")]
     [Test]
-    public void GetGeneratedMember_ClassMembers ()
+    public void GetGeneratedMember_Members ()
     {
-      MethodInfo method = null;
       FieldInfo field = null;
+      ConstructorInfo ctor = null;
+      MethodInfo method = null;
       PropertyInfo property = null;
       EventInfo event_ = null;
 
@@ -68,6 +71,7 @@ namespace Remotion.TypePipe.IntegrationTests.TypeAssembly
             var proxyType = typeContext.ProxyType;
 
             var addedField = proxyType.AddField ("MyField", FieldAttributes.Public, typeof (int));
+            var addedCtor = proxyType.AddedConstructors.Single();
             var addedMethod = proxyType.AddMethod (
                 "MyMethod", MethodAttributes.Public, typeof (void), ParameterDeclaration.None, ctx => Expression.Empty());
             var addedProperty = proxyType.AddProperty (
@@ -78,30 +82,19 @@ namespace Remotion.TypePipe.IntegrationTests.TypeAssembly
             typeContext.GenerationCompleted +=
                 ctx =>
                 {
-                  method = (MethodInfo) ctx.GetGeneratedMember (addedMethod);
                   field = (FieldInfo) ctx.GetGeneratedMember (addedField);
+                  ctor = (ConstructorInfo) ctx.GetGeneratedMember (addedCtor);
+                  method = (MethodInfo) ctx.GetGeneratedMember (addedMethod);
                   event_ = (EventInfo) ctx.GetGeneratedMember (addedEvent);
                   property = (PropertyInfo) ctx.GetGeneratedMember (addedProperty);
                 };
           });
 
       Assert.That (field, Is.SameAs (type.GetFields().Single()));
-      Assert.That (field.Name, Is.EqualTo ("MyField"));
-      Assert.That (field.FieldType, Is.SameAs (typeof (int)));
-
       Assert.That (method, Is.SameAs (type.GetMethods().Single (m => m.Name == "MyMethod")));
-      Assert.That (method.ReturnType, Is.SameAs (typeof (void)));
-      Assert.That (method.Attributes, Is.EqualTo (MethodAttributes.Public));
-      Assert.That (method.GetParameters(), Is.Empty);
-
+      Assert.That (ctor, Is.SameAs (type.GetConstructors().Single()));
       Assert.That (property, Is.SameAs (type.GetProperties().Single()));
-      Assert.That (property.Name, Is.EqualTo ("MyProperty"));
-      Assert.That (property.PropertyType, Is.SameAs (typeof (string)));
-      Assert.That (property.GetIndexParameters(), Is.Empty);
-
       Assert.That (event_, Is.SameAs (type.GetEvents().Single()));
-      Assert.That (event_.Name, Is.EqualTo ("MyEvent"));
-      Assert.That (event_.EventHandlerType, Is.SameAs (typeof (Action)));
     }
 
     public class DomainType {}
