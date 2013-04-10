c1620b2880549f2bf4856cc9813fca96efc296ce
Julian Lettner <julian.lettner@rubicon.eu>
2013-04-10 11:45:06 +0000

    Reverted commit: 26864

--- Remotion/TypePipe/Core/CodeGeneration/ITypeAssembler.cs
+++ Remotion/TypePipe/Core/CodeGeneration/ITypeAssembler.cs
@@ -26,7 +26,7 @@ namespace Remotion.TypePipe.CodeGeneration
   /// <summary>
   /// Generates types for requested types and computes compound cache keys to enabled efficient caching of generated types.
   /// </summary>
-  public interface ITypeAssembler : IReflectionService
+  public interface ITypeAssembler
   {
     string ParticipantConfigurationID { get; }
     ReadOnlyCollection<IParticipant> Participants { get; }
@@ -44,6 +44,8 @@ namespace Remotion.TypePipe.CodeGeneration
     object[] GetCompoundCacheKey (Func<ICacheKeyProvider, Type, object> cacheKeyProviderMethod, Type type, int freeSlotsAtStart);
 
     Type AssembleType (Type requestedType, IDictionary<string, object> participantState, IMutableTypeBatchCodeGenerator codeGenerator);
+
+    bool IsAssembledType (Type type);
 
     void RebuildParticipantState (LoadedTypesContext loadedTypesContext);
   }
