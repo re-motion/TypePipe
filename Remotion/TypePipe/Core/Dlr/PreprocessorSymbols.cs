971b4bb3c5b7ba8c15fda7b5e80a7e2b6811ced3
Julian Lettner <julian.lettner@rubicon.eu>
2013-04-22 14:23:56 +0000

    Added safe-guard for CLR2 compilation symbol. (RM-5370)

--- Remotion/TypePipe/Core/Dlr/PreprocessorSymbols.cs
+++ Remotion/TypePipe/Core/Dlr/PreprocessorSymbols.cs
@@ -1,4 +1,6 @@
 ﻿// This is required so that the DLR files compile in CLR 2 mode - the expressions have a namespace of Microsoft.Scripting.Ast, rather than 
 // System.Linq.Expressions.
 // Use this flag even with .NET 4 to avoid namespace clashes.
-#define CLR2
+#if !CLR2
+  ERROR: You must use the 'CLR2' conditional compilation symbol to avoid namespace clashes.
+#endif
