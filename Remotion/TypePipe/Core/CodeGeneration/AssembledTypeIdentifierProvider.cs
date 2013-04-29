592cfe351bbe029197f3abae99d83f167e721c94
Julian Lettner <julian.lettner@rubicon.eu>
2013-04-29 15:37:50 +0000

    Added IAssembledTypeIdentifierProvider. (RM-5552)

---
+++ Remotion/TypePipe/Core/CodeGeneration/AssembledTypeIdentifierProvider.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Remotion.TypePipe.Caching;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.CodeGeneration
+{
+  /// <summary>
+  /// Provides identifiers for assembled types.
+  /// </summary>
+  public class AssembledTypeIdentifierProvider : IAssembledTypeIdentifierProvider
+  {
+    // Array for performance reasons.
+    private readonly ITypeIdentifierProvider[] _identifierProviders;
+    private readonly Dictionary<IParticipant, int> _identifierProviderIndexes;
+
+    public AssembledTypeIdentifierProvider (IEnumerable<IParticipant> participants)
+    {
+      ArgumentUtility.CheckNotNull ("participants", participants);
+
+      var providersWithIndex = participants
+          .Select (p => new { Participant = p, IdentifierProvider = p.PartialTypeIdentifierProvider })
+          .Where (t => t.IdentifierProvider != null)
+          .Select ((t, i) => new { t.Participant, t.IdentifierProvider, Index = i }).ToList();
+
+      _identifierProviders = providersWithIndex.Select (t => t.IdentifierProvider).ToArray();
+      _identifierProviderIndexes = providersWithIndex.ToDictionary (t => t.Participant, t => t.Index);
+    }
+
+    public object[] GetIdentifier (Type requestedType)
+    {
+      // Using Debug.Assert because it will be compiled away.
+      Debug.Assert (requestedType != null);
+
+      var id = new object[_identifierProviders.Length + 1];
+      id[0] = requestedType;
+
+      // No LINQ for performance reasons.
+      for (int i = 0; i < _identifierProviders.Length; i++)
+        id[i + 1] = _identifierProviders[i].GetID (requestedType);
+
+      return id;
+    }
+
+    public object GetPartialIdentifier (object[] identifier, IParticipant participant)
+    {
+      ArgumentUtility.CheckNotNull ("identifier", identifier);
+      ArgumentUtility.CheckNotNull ("participant", participant);
+
+      int index;
+      if (_identifierProviderIndexes.TryGetValue (participant, out index))
+        return identifier[index + 1];
+
+      return null;
+    }
+  }
+}
---
+++ Remotion/TypePipe/UnitTests/CodeGeneration/AssembledTypeIdentifierProviderTest.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+
+using NUnit.Framework;
+using Remotion.Development.UnitTesting.Reflection;
+using Remotion.TypePipe.Caching;
+using Remotion.TypePipe.CodeGeneration;
+using Rhino.Mocks;
+using Remotion.Development.UnitTesting.Enumerables;
+
+namespace Remotion.TypePipe.UnitTests.CodeGeneration
+{
+  [TestFixture]
+  public class AssembledTypeIdentifierProviderTest
+  {
+    private IParticipant _participantWithoutIdentifierProvider;
+    private IParticipant _participantWithIdentifierProvider;
+    private ITypeIdentifierProvider _identifierProviderStub;
+
+    private AssembledTypeIdentifierProvider _provider;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _participantWithoutIdentifierProvider = MockRepository.GenerateStub<IParticipant>();
+      _participantWithIdentifierProvider = MockRepository.GenerateStub<IParticipant>();
+      _identifierProviderStub = MockRepository.GenerateStub<ITypeIdentifierProvider>();
+      _participantWithIdentifierProvider.Stub (_ => _.PartialTypeIdentifierProvider).Return (_identifierProviderStub);
+
+      _provider = new AssembledTypeIdentifierProvider (new[] { _participantWithoutIdentifierProvider, _participantWithIdentifierProvider }.AsOneTime());
+    }
+
+    [Test]
+    public void GetIdentifier ()
+    {
+      var requestedType = ReflectionObjectMother.GetSomeType();
+      _identifierProviderStub.Stub (_ => _.GetID (requestedType)).Return ("abc");
+
+      var result = _provider.GetIdentifier (requestedType);
+
+      Assert.That (result, Is.EqualTo (new object[] { requestedType, "abc" }));
+    }
+
+    [Test]
+    public void GetPartialIdentifier_ParticipantDoesNotContributeToIdentifier ()
+    {
+      var identifier = new object[0];
+
+      var result = _provider.GetPartialIdentifier (identifier, _participantWithoutIdentifierProvider);
+
+      Assert.That (result, Is.Null);
+    }
+
+    [Test]
+    public void GetPartialIdentifier_ParticipantContributesToIdentifier ()
+    {
+      var identifier = new object[] { "requestedType", "abc", "def" };
+
+      var result = _provider.GetPartialIdentifier (identifier, _participantWithIdentifierProvider);
+
+      Assert.That (result, Is.EqualTo ("abc"));
+    }
+  }
+}
