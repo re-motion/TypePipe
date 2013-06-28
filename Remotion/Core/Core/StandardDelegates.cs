5d7af7ade7d067b36568034abecc1c6a5a320740
Michael Ketting <michael.ketting@rubicon.eu>
2013-06-28 11:36:09 +0000

    Dropped Action<> and Func<> overloads already part of .NET 4.5. (RM-5563)

--- Remotion/Core/Core/StandardDelegates.cs
+++ Remotion/Core/Core/StandardDelegates.cs
@@ -20,26 +20,6 @@ using System.ComponentModel;
 namespace Remotion
 {
   /// <summary>
-  /// Provides a standard delegate type for an action without return value
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate void Action<T1, T2, T3, T4, T5> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
-  /// <summary>
-  /// Provides a standard delegate type for an action without return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate void Action<T1, T2, T3, T4, T5, T6> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
-  /// <summary>
-  /// Provides a standard delegate type for an action without return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate void Action<T1, T2, T3, T4, T5, T6, T7> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
-  /// <summary>
-  /// Provides a standard delegate type for an action without return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate void Action<T1, T2, T3, T4, T5, T6, T7, T8> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8);
-  /// <summary>
   /// Provides a standard delegate type for an action without return value.
   /// </summary>
   [EditorBrowsable (EditorBrowsableState.Never)]
@@ -100,26 +80,6 @@ namespace Remotion
   [EditorBrowsable (EditorBrowsableState.Always)]
   public delegate void Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16, T17 arg17, T18 arg18, T19 arg19, T20 arg20);
 
-  /// <summary>
-  /// Provides a standard delegate type for a function with a return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate TResult Func<T1, T2, T3, T4, T5, TResult> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
-  /// <summary>
-  /// Provides a standard delegate type for a function with a return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate TResult Func<T1, T2, T3, T4, T5, T6, TResult> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
-  /// <summary>
-  /// Provides a standard delegate type for a function with a return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate TResult Func<T1, T2, T3, T4, T5, T6, T7, TResult> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
-  /// <summary>
-  /// Provides a standard delegate type for a function with a return value.
-  /// </summary>
-  [EditorBrowsable (EditorBrowsableState.Never)]
-  public delegate TResult Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8);
   /// <summary>
   /// Provides a standard delegate type for a function with a return value.
   /// </summary>
