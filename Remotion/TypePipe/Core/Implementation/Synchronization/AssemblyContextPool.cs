bcb833dca1245705056172ac7a0a29cf7f612b58
Michael Ketting <michael.ketting@rubicon.eu>
2013-09-09 12:46:25 +0000

    temp commit design for AssemblyContextPool. (RM-5840)

---
+++ Remotion/TypePipe/Core/Implementation/Synchronization/AssemblyContextPool.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under 
+// the Apache License, Version 2.0 (the "License"); you may not use this 
+// file except in compliance with the License.  You may obtain a copy of the 
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
+// License for the specific language governing permissions and limitations
+// under the License.
+// 
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Remotion.TypePipe.CodeGeneration;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.Implementation.Synchronization
+{
+  public class AssemblyContextPool : IAssemblyContextPool
+  {
+    //TODO 5840: Tests
+    //TODO 5840: Docs
+    private readonly BlockingCollection<AssemblyContext> _queue = new BlockingCollection<AssemblyContext> (new ConcurrentQueue<AssemblyContext>());
+
+    // Thread-safe set (for multiple readers, no writer).
+    private readonly Dictionary<AssemblyContext, AssemblyContext> _allContexts;
+
+    public AssemblyContextPool (IEnumerable<AssemblyContext> assemblyContexts)
+    {
+      ArgumentUtility.CheckNotNull ("assemblyContexts", assemblyContexts);
+
+      _allContexts = assemblyContexts.ToDictionary (c => c);
+    }
+
+    public AssemblyContext[] DequeueAll ()
+    {
+      return _queue.GetConsumingEnumerable().Take (_allContexts.Count).ToArray();
+    }
+
+    public void Enqueue (AssemblyContext context)
+    {
+      ArgumentUtility.CheckNotNull ("context", context);
+
+      if (!_allContexts.ContainsKey (context))
+        throw new ArgumentException();
+
+      _queue.Add (context);
+    }
+
+    public AssemblyContext Dequeue ()
+    {
+      return _queue.Take();
+    }
+  }
+}
--- Remotion/TypePipe/Core/Implementation/Synchronization/SynchronizationPoint.cs
+++ Remotion/TypePipe/Core/Implementation/Synchronization/SynchronizationPoint.cs
@@ -19,9 +19,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
 using Remotion.TypePipe.Caching;
-using Remotion.TypePipe.CodeGeneration;
 using Remotion.TypePipe.MutableReflection;
 using Remotion.TypePipe.TypeAssembly.Implementation;
 using Remotion.Utilities;
@@ -41,42 +39,40 @@ namespace Remotion.TypePipe.Implementation.Synchronization
     {
       ArgumentUtility.CheckNotNull ("typeAssembler", typeAssembler);
       ArgumentUtility.CheckNotNull ("assemblyContextPool", assemblyContextPool);
-      
+
       _typeAssembler = typeAssembler;
       _assemblyContextPool = assemblyContextPool;
     }
 
-    //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
     public string AssemblyDirectory
     {
-      get { lock (_codeGenerationLock) return _assemblyContext.GeneratedCodeFlusher.AssemblyDirectory; }
+      //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
+      get { throw new NotSupportedException ("TODO 5840"); }
     }
 
-    //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
     public string AssemblyNamePattern
     {
-      get { lock (_codeGenerationLock) return _assemblyContext.GeneratedCodeFlusher.AssemblyNamePattern; }
+      //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
+      get { throw new NotSupportedException ("TODO 5840"); }
     }
 
-    //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
     public void SetAssemblyDirectory (string assemblyDirectory)
     {
-      lock (_codeGenerationLock)
-        _assemblyContext.GeneratedCodeFlusher.SetAssemblyDirectory (assemblyDirectory);
+      //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
+      throw new NotSupportedException ("TODO 5840");
     }
 
-    //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
     public void SetAssemblyNamePattern (string assemblyNamePattern)
     {
-      lock (_codeGenerationLock)
-        _assemblyContext.GeneratedCodeFlusher.SetAssemblyNamePattern (assemblyNamePattern);
+      //TODO 5840: Setting the AssemblyDirectory to PipelineFactory.Settings
+      throw new NotSupportedException ("TODO 5840");
     }
 
-    //TODO 5840: Affects all AssemblyContexts in the pool.
-    public string[] FlushCodeToDisk (IEnumerable<CustomAttributeDeclaration> assemblyAttributes)
+    public string FlushCodeToDisk (IEnumerable<CustomAttributeDeclaration> assemblyAttributes)
     {
-      // Dequeue all contexts, flush them, enqueue again.
-      return _assemblyContextPool.FlushAll (assemblyAttributes);
+      //TODO 5840: Affects all AssemblyContexts in the pool.
+      // Dequeue all contexts, flush them, enqueue again and return string[] for the flushed assembly paths.
+      throw new NotSupportedException ("TODO 5840");
     }
 
     // TODO 5840: Move out from SyncPoint to caller.
@@ -107,22 +103,25 @@ namespace Remotion.TypePipe.Implementation.Synchronization
     {
       ArgumentUtility.CheckNotNull ("types", types);
 
-      Type generatedType;
-      var assemblyContext = _assemblyContextPool.DequeueNextAssemblyContext();
+      // TODO 5840: Move to TypeCache
+      // TODO 5840: Test Dequeue.
+      // TODO 5840: Test Enqueue in finally-block.
+      var assemblyContext = _assemblyContextPool.Dequeue();
       try
       {
+        Type generatedType;
         if (types.TryGetValue (typeID, out generatedType))
           return generatedType;
 
         generatedType = _typeAssembler.AssembleType (typeID, assemblyContext.ParticipantState, assemblyContext.MutableTypeBatchCodeGenerator);
         AddTo (types, typeID, generatedType);
+
+        return generatedType;
       }
       finally
       {
-        _assemblyContextPool.EnqueueAssemblyContext (assemblyContext);
+        _assemblyContextPool.Enqueue (assemblyContext);
       }
-
-      return generatedType;
     }
 
     public void RebuildParticipantState (
@@ -134,17 +133,25 @@ namespace Remotion.TypePipe.Implementation.Synchronization
       ArgumentUtility.CheckNotNull ("keysToAssembledTypes", keysToAssembledTypes);
       ArgumentUtility.CheckNotNull ("additionalTypes", additionalTypes);
 
-      var loadedAssembledTypes = new List<Type>();
-
-      lock (_codeGenerationLock)
+      // TODO 5840: Move to TypeCache
+      // TODO 5840: Test Dequeue.
+      // TODO 5840: Test Enqueue in finally-block.
+      // TODO 5840: Figure out if this should be performed for all AssemblyContexts in the Pool, or only once but still locking all AssemblyContexts.
+      var assemblyContext = _assemblyContextPool.Dequeue();
+      try
       {
+        var loadedAssembledTypes = new List<Type>();
         foreach (var p in keysToAssembledTypes.Where (p => !types.ContainsKey (p.Key)))
         {
           AddTo (types, p.Key, p.Value);
           loadedAssembledTypes.Add (p.Value);
         }
 
-        _typeAssembler.RebuildParticipantState (loadedAssembledTypes, additionalTypes, _assemblyContext.ParticipantState);
+        _typeAssembler.RebuildParticipantState (loadedAssembledTypes, additionalTypes, assemblyContext.ParticipantState);
+      }
+      finally
+      {
+        _assemblyContextPool.Enqueue (assemblyContext);
       }
     }
 
@@ -152,12 +159,20 @@ namespace Remotion.TypePipe.Implementation.Synchronization
     {
       ArgumentUtility.CheckNotNull ("additionalTypeID", additionalTypeID);
 
-      lock (_codeGenerationLock)
+      // TODO 5840: Move to TypeCache
+      // TODO 5840: Test Dequeue.
+      // TODO 5840: Test Enqueue in finally-block.
+      var assemblyContext = _assemblyContextPool.Dequeue();
+      try
       {
         return _typeAssembler.GetOrAssembleAdditionalType (
             additionalTypeID,
-            _assemblyContext.ParticipantState,
-            _assemblyContext.MutableTypeBatchCodeGenerator);
+            assemblyContext.ParticipantState,
+            assemblyContext.MutableTypeBatchCodeGenerator);
+      }
+      finally
+      {
+        _assemblyContextPool.Enqueue (assemblyContext);
       }
     }
 
@@ -165,46 +180,6 @@ namespace Remotion.TypePipe.Implementation.Synchronization
     {
       if (!concurrentDictionary.TryAdd (key, value))
         throw new ArgumentException ("Key already exists.");
-    }
-  }
-
-  public interface IAssemblyContextPool
-  {
-    AssemblyContext[] DequeueAll ();
-    void Enqueue (AssemblyContext context);
-    AssemblyContext Dequeue ();
-  }
-
-  class AssemblyContextPool : IAssemblyContextPool
-  {
-    private readonly BlockingCollection<AssemblyContext> _queue = new BlockingCollection<AssemblyContext>(new ConcurrentQueue<AssemblyContext>());
-    
-    // Thread-safe set (for multiple readers, no writer).
-    private readonly Dictionary<AssemblyContext, AssemblyContext> _allContexts;
-
-    public AssemblyContextPool (IEnumerable<AssemblyContext> assemblyContexts)
-    {
-      _allContexts = assemblyContexts.ToDictionary(c => c);
-    }
-
-    public AssemblyContext[] DequeueAll ()
-    {
-      return _queue.GetConsumingEnumerable().Take (_allContexts.Count).ToArray();
-    }
-
-    public void Enqueue (AssemblyContext context)
-    {
-      ArgumentUtility.CheckNotNull ("context", context);
-
-      if (!_allContexts.ContainsKey (context))
-        throw new ArgumentException();
-
-      _queue.Add(context);
-    }
-
-    public AssemblyContext Dequeue ()
-    {
-      return _queue.Take();
     }
   }
 }
