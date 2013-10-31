0466099c6ebad16e52f8c7083af30084c720ebe4
Michael Ketting <michael.ketting@rubicon.eu>
2013-10-31 16:13:03 +0000

    Implemented ThreadLocalAssemblyContextPoolDecorator. (RM-5890)

---
+++ Remotion/TypePipe/Core/CodeGeneration/ThreadLocalAssemblyContextPoolDecorator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using Remotion.Utilities;
+
+namespace Remotion.TypePipe.CodeGeneration
+{
+  /// <summary>
+  /// Decorates the <see cref="IAssemblyContextPool"/> with a thread-local cache. This allows the reuse of the same assembly context within a thread.
+  /// </summary>
+  public class ThreadLocalAssemblyContextPoolDecorator : IAssemblyContextPool
+  {
+    private class ThreadLocalAssemblyContext
+    {
+      private readonly AssemblyContext _value;
+      private int _count;
+
+      public ThreadLocalAssemblyContext (AssemblyContext value)
+      {
+        _value = value;
+      }
+
+      public AssemblyContext Value
+      {
+        get { return _value; }
+      }
+
+      public int Count
+      {
+        get { return _count; }
+      }
+
+      public void IncrementCount ()
+      {
+        _count++;
+      }
+
+      public void DecrementCount ()
+      {
+        Assertion.IsTrue (_count > 0, "Count can never be decremented past 0.");
+        _count--;
+      }
+    }
+
+    private readonly string _instanceID = Guid.NewGuid().ToString();
+    private readonly IAssemblyContextPool _assemblyContextPool;
+
+    public ThreadLocalAssemblyContextPoolDecorator (IAssemblyContextPool assemblyContextPool)
+    {
+      _assemblyContextPool = assemblyContextPool;
+    }
+
+    public void Enqueue (AssemblyContext assemblyContext)
+    {
+      ArgumentUtility.CheckNotNull ("assemblyContext", assemblyContext);
+
+      var threadLocalAssemblyContext = (ThreadLocalAssemblyContext) CallContext.GetData (_instanceID);
+      if (threadLocalAssemblyContext == null)
+      {
+        throw new InvalidOperationException (
+            "No AssemblyContext has been dequeued on the current thread. "
+            + "An AssemblyContext must be enqueued on the same thread it was dequeued from.");
+      }
+
+      if (threadLocalAssemblyContext.Value != assemblyContext)
+      {
+        throw new InvalidOperationException (
+            "The specified AssemblyContext has been dequeued on a different thread. "
+            + "An AssemblyContext must be enqueued on the same thread it was dequeued from.");
+      }
+
+      threadLocalAssemblyContext.DecrementCount();
+
+      if (threadLocalAssemblyContext.Count == 0)
+      {
+        try
+        {
+          _assemblyContextPool.Enqueue (assemblyContext);
+        }
+        catch
+        {
+          threadLocalAssemblyContext.IncrementCount();
+          throw;
+        }
+
+        CallContext.FreeNamedDataSlot (_instanceID);
+      }
+    }
+
+    public AssemblyContext Dequeue ()
+    {
+      var threadLocalAssemblyContext = (ThreadLocalAssemblyContext) CallContext.GetData (_instanceID);
+      if (threadLocalAssemblyContext == null)
+      {
+        var assemblyContext = _assemblyContextPool.Dequeue();
+        threadLocalAssemblyContext = new ThreadLocalAssemblyContext (assemblyContext);
+        CallContext.SetData (_instanceID, threadLocalAssemblyContext);
+      }
+
+      threadLocalAssemblyContext.IncrementCount();
+      return threadLocalAssemblyContext.Value;
+    }
+
+    public AssemblyContext[] DequeueAll ()
+    {
+      return _assemblyContextPool.DequeueAll();
+    }
+  }
+}
---
+++ Remotion/TypePipe/UnitTests/CodeGeneration/ThreadLocalAssemblyContextPoolDecoratorTest.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using NUnit.Framework;
+using Remotion.TypePipe.CodeGeneration;
+using Rhino.Mocks;
+
+namespace Remotion.TypePipe.UnitTests.CodeGeneration
+{
+  [TestFixture]
+  public class ThreadLocalAssemblyContextPoolDecoratorTest
+  {
+    private IAssemblyContextPool _assemblyContextPoolMock;
+    private ThreadLocalAssemblyContextPoolDecorator _decorator;
+    private AssemblyContext _assemblyContext;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _assemblyContextPoolMock = MockRepository.GenerateStrictMock<IAssemblyContextPool>();
+      _assemblyContext = new AssemblyContext (
+          MockRepository.GenerateStrictMock<IMutableTypeBatchCodeGenerator>(),
+          MockRepository.GenerateStrictMock<IGeneratedCodeFlusher>());
+
+      _decorator = new ThreadLocalAssemblyContextPoolDecorator (_assemblyContextPoolMock);
+    }
+
+    [Test]
+    public void DequeueAll_ReturnsAssemblyContextsFromInnerPool ()
+    {
+      AssemblyContext[] expected = { _assemblyContext };
+      _assemblyContextPoolMock.Expect (mock => mock.DequeueAll()).Return (expected);
+
+      var actual = _decorator.DequeueAll();
+
+      Assert.That (actual, Is.SameAs (expected));
+    }
+
+    [Test]
+    public void Dequeue_Once_ReturnsAssemblyContextFromInnerPool ()
+    {
+      _assemblyContextPoolMock.Expect (mock => mock.Dequeue()).Return (_assemblyContext);
+
+      var actual = _decorator.Dequeue();
+
+      Assert.That (actual, Is.SameAs (_assemblyContext));
+    }
+
+    [Test]
+    public void Dequeue_TwiceOnSameThread_ReturnsAssemblyContextFromThreadLocalCacheOnSecondCall ()
+    {
+      var hasDequeued = false;
+      _assemblyContextPoolMock.Expect (mock => mock.Dequeue())
+          .WhenCalled (mi => { hasDequeued = true; })
+          .Return (_assemblyContext);
+
+      var actual1 = _decorator.Dequeue();
+      Assert.That (hasDequeued, Is.True);
+
+      hasDequeued = false;
+      var actual2 = _decorator.Dequeue();
+      Assert.That (hasDequeued, Is.False);
+
+      Assert.That (actual2, Is.SameAs (actual1));
+    }
+
+    [Test]
+    public void Enqueue_AfterDequeue_PassesAssemblyContextToInnerPool ()
+    {
+      _assemblyContextPoolMock.Expect (mock => mock.Dequeue()).Return (_assemblyContext);
+      _assemblyContextPoolMock.Expect (mock => mock.Enqueue (_assemblyContext));
+
+      var expected = _decorator.Dequeue();
+
+      _decorator.Enqueue (expected);
+
+      _assemblyContextPoolMock.VerifyAllExpectations();
+    }
+
+    [Test]
+    public void Enqueue_TwiceAfterTwoDequeues_PassesAssemblyContextToInnerPoolOnSecondCall ()
+    {
+      var hasEnqueued = false;
+      _assemblyContextPoolMock.Expect (mock => mock.Dequeue()).Return (_assemblyContext);
+      _assemblyContextPoolMock
+          .Expect (mock => mock.Enqueue (_assemblyContext))
+          .WhenCalled (mi => { hasEnqueued = true; });
+
+      var expected1 = _decorator.Dequeue();
+      var expected2 = _decorator.Dequeue();
+
+      _decorator.Enqueue (expected1);
+      Assert.That (hasEnqueued, Is.False);
+
+      _decorator.Enqueue (expected2);
+      Assert.That (hasEnqueued, Is.True);
+
+      _assemblyContextPoolMock.VerifyAllExpectations();
+    }
+
+    [Test]
+    public void Enqueue_WithoutDequeue_ThrowsInvalidOperationException ()
+    {
+      Assert.That (
+          () => _decorator.Enqueue (_assemblyContext),
+          Throws.InvalidOperationException.And.Message.EqualTo (
+              "No AssemblyContext has been dequeued on the current thread. "
+              + "An AssemblyContext must be enqueued on the same thread it was dequeued from."));
+    }
+
+    [Test]
+    public void Enqueue_WithAssemblyContextFromOtherThread_ThrowsInvalidOperationException ()
+    {
+      _assemblyContextPoolMock.Expect (mock => mock.Dequeue()).Return (_assemblyContext);
+
+      _decorator.Dequeue();
+
+      var otherAssemblyContext = new AssemblyContext (
+          MockRepository.GenerateStrictMock<IMutableTypeBatchCodeGenerator>(),
+          MockRepository.GenerateStrictMock<IGeneratedCodeFlusher>());
+
+      Assert.That (
+          () => _decorator.Enqueue (otherAssemblyContext),
+          Throws.InvalidOperationException.And.Message.EqualTo (
+              "The specified AssemblyContext has been dequeued on a different thread. "
+              + "An AssemblyContext must be enqueued on the same thread it was dequeued from."));
+    }
+
+    [Test]
+    public void Enqueue_IsStillSupportedAfterInnerPoolEnqueueFails ()
+    {
+      var exception = new Exception();
+      var hasEnqueued = false;
+
+      _assemblyContextPoolMock.Expect (mock => mock.Dequeue()).Return (_assemblyContext);
+      _assemblyContextPoolMock
+          .Expect (mock => mock.Enqueue (_assemblyContext))
+          .Throw (exception);
+      _assemblyContextPoolMock
+          .Expect (mock => mock.Enqueue (_assemblyContext))
+          .WhenCalled (mi => { hasEnqueued = true; });
+
+      var expected = _decorator.Dequeue();
+
+      Assert.That (() => _decorator.Enqueue (expected), Throws.Exception.SameAs (exception));
+      Assert.That (hasEnqueued, Is.False);
+
+      Assert.That (() => _decorator.Enqueue (expected), Throws.Nothing);
+      Assert.That (hasEnqueued, Is.True);
+
+      _assemblyContextPoolMock.VerifyAllExpectations();
+    }
+  }
+}
