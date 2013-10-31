a99f1e94e83ac871e66944bb5663a0df172182b1
Michael Ketting <michael.ketting@rubicon.eu>
2013-10-31 07:56:04 +0000

    Added ConstructorCallCache. (RM-5884)

---
+++ Remotion/TypePipe/Core/Caching/IConstructorCallCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Remotion.TypePipe.Caching
+{
+  /// <summary>
+  /// Caches the <see cref="Delegate"/>s that perform constructor calls for pipeline generated <see cref="Type"/>s to enable efficient object creation.
+  /// </summary>
+  /// <threadsafety static="true" instance="true"/>
+  public interface IConstructorCallCache
+  {
+    Delegate GetOrCreateConstructorCall (AssembledTypeID typeID, Type delegateType, bool allowNonPublic);
+  }
+}
