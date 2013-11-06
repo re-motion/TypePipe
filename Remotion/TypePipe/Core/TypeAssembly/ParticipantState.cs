52c92aabe4ec85f902b5d201a8414984ad88ecec
Michael Ketting <michael.ketting@rubicon.eu>
2013-11-06 13:44:53 +0000

    Introduced ParticipantState instead of Dictionary for the per-AssemblyContext state. (RM-5899)

--- Remotion/TypePipe/Core/TypeAssembly/ParticipantState.cs
+++ Remotion/TypePipe/Core/TypeAssembly/ParticipantState.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using Remotion.TypePipe.TypeAssembly.Implementation;
+using Remotion.Utilities;
 
 namespace Remotion.TypePipe.TypeAssembly
 {
+  ///<summary>Default implementation of the <see cref="IParticipantState"/> interface.</summary>
   /// <threadsafety static="true" instance="false"/>
   public class ParticipantState : IParticipantState
   {
-    // TODO RM-5895: test
-
     private readonly Dictionary<string, object> _state = new Dictionary<string, object>();
 
     public ParticipantState ()
@@ -17,11 +16,19 @@ namespace Remotion.TypePipe.TypeAssembly
 
     public void AddState (string id, object value)
     {
+      ArgumentUtility.CheckNotNullOrEmpty ("id", id);
+      ArgumentUtility.CheckNotNull ("value", value);
+
+      if (_state.ContainsKey (id))
+        throw new InvalidOperationException (string.Format ("State identified by the id '{0}' already exists. State identifier must be unique.", id));
+
       _state.Add (id, value);
     }
 
     public object GetState (string id)
     {
+      ArgumentUtility.CheckNotNullOrEmpty ("id", id);
+
       object value;
       if (_state.TryGetValue (id, out value))
         return value;
---
+++ Remotion/TypePipe/UnitTests/TypeAssembly/ParticipantStateTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Remotion.TypePipe.TypeAssembly;
+
+namespace Remotion.TypePipe.UnitTests.TypeAssembly
+{
+  [TestFixture]
+  public class ParticipantStateTest
+  {
+    [Test]
+    public void GetState_WithIDForAddedValue_ReturnsValue ()
+    {
+      var participantState = new ParticipantState();
+      var value = new object();
+      participantState.AddState ("TheID", value);
+
+      Assert.That (participantState.GetState ("TheID"), Is.SameAs (value));
+    }
+
+    [Test]
+    public void GetState_WithUnknownID_ReturnsNull ()
+    {
+      var participantState = new ParticipantState();
+
+      Assert.That (participantState.GetState ("UnknownID"), Is.Null);
+    }
+
+    [Test]
+    public void AddState_WithExistingID_ThrowsInvalidOperationException ()
+    {
+      var participantState = new ParticipantState();
+      participantState.AddState ("TheID", new object());
+
+      Assert.That (
+          () => participantState.AddState ("TheID", new object()),
+          Throws.InvalidOperationException
+              .With.Message.EqualTo ("State identified by the id 'TheID' already exists. State identifier must be unique."));
+    }
+  }
+}
