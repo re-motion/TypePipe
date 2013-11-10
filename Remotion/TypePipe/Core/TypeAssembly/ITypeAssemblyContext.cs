475b5b26e5cbf75849b422cefa96cb0213a2770b
Michael Ketting <michael.ketting@rubicon.eu>
2013-11-10 16:48:08 +0000

    Namespace cleanup.

--- Remotion/TypePipe/Core/TypeAssembly/ITypeAssemblyContext.cs
+++ Remotion/TypePipe/Core/TypeAssembly/ITypeAssemblyContext.cs
@@ -17,7 +17,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Reflection;
 using JetBrains.Annotations;
 using Remotion.TypePipe.MutableReflection;
