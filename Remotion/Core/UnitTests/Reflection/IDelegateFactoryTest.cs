01436a4e08efb6e7a86e2015365b022d06366a65
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2013-12-12 11:54:08 +0000

    added missing interface tests

---
+++ Remotion/Core/UnitTests/Reflection/IDelegateFactoryTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Remotion.Reflection;
+using Remotion.ServiceLocation;
+
+namespace Remotion.UnitTests.Reflection
+{
+  public class IDelegateFactoryTest
+  {
+    private DefaultServiceLocator _serviceLocator;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _serviceLocator = new DefaultServiceLocator();
+    }
+
+    [Test]
+    public void GetInstance_Once ()
+    {
+      var factory = _serviceLocator.GetInstance<IDelegateFactory>();
+
+      Assert.That (factory, Is.Not.Null);
+      Assert.That (factory, Is.TypeOf (typeof (DelegateFactory)));
+    }
+
+    [Test]
+    public void GetInstance_Twice ()
+    {
+      var factory1 = _serviceLocator.GetInstance<IDelegateFactory>();
+      var factory2 = _serviceLocator.GetInstance<IDelegateFactory>();
+
+      Assert.That (factory1, Is.Not.SameAs (factory2));
+    }
+  }
+}
---
+++ Remotion/TypePipe/UnitTests/MutableReflection/Implementation/ICustomAttributeDataRetrieverTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Remotion.ServiceLocation;
+using Remotion.TypePipe.MutableReflection.Implementation;
+
+namespace Remotion.TypePipe.UnitTests.MutableReflection.Implementation
+{
+  [TestFixture]
+  public class ICustomAttributeDataRetrieverTest
+  {
+    private DefaultServiceLocator _serviceLocator;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _serviceLocator = new DefaultServiceLocator();
+    }
+
+    [Test]
+    public void GetInstance_Once ()
+    {
+      var factory = _serviceLocator.GetInstance<ICustomAttributeDataRetriever>();
+
+      Assert.That (factory, Is.Not.Null);
+      Assert.That (factory, Is.TypeOf (typeof (CustomAttributeDataRetriever)));
+    }
+
+    [Test]
+    public void GetInstance_Twice ()
+    {
+      var factory1 = _serviceLocator.GetInstance<ICustomAttributeDataRetriever>();
+      var factory2 = _serviceLocator.GetInstance<ICustomAttributeDataRetriever>();
+
+      Assert.That (factory1, Is.Not.SameAs (factory2));
+    }
+  }
+}
