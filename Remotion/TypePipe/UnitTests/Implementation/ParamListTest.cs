ad25ffa766a2fd11b45f975dd8f13596f174060f
Michael Ketting <michael.ketting@rubicon.eu>
2013-12-23 14:48:34 +0000

    Move ParamList from Remotion.dll to Remotion.TypePipe.dll (RM-5963)

--- Remotion/TypePipe/UnitTests/Implementation/ParamListTest.cs
+++ Remotion/TypePipe/UnitTests/Implementation/ParamListTest.cs
@@ -14,10 +14,11 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+
 using System;
 using NUnit.Framework;
 
-namespace Remotion.TypePipe.UnitTests
+namespace Remotion.TypePipe.UnitTests.Implementation
 {
   [TestFixture]
   public class ParamListTest
