adafb33b0db4e54df65254098600e979e49d3704
re-motion Build User <FX-Team@rubicon.eu>
2014-04-21 11:17:23 +0000

    Comitting AssemblyInfos for local build after setting version to '1.15.13.10000'.

--- TypePipe/AssemblyInfoShared.cs
+++ TypePipe/AssemblyInfoShared.cs
@@ -28,7 +28,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("re-motion code generation library (typepipe.codeplex.com)")]
 [assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under Apache License, Version 2.0")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("1.15.12.10000  Debug; ")]
+[assembly: AssemblyInformationalVersion ("1.15.13.10000  Debug; ")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -41,5 +41,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.15.12.10000")]
-[assembly: AssemblyFileVersion ("1.15.12.10000")]
+[assembly: AssemblyVersion("1.15.13.10000")]
+[assembly: AssemblyFileVersion ("1.15.13.10000")]
