3db6a61df94a70f77dcafaf94196232178eeaa45
Dominik Rauch <dominik.rauch@rubicon.eu>
2014-10-08 07:38:42 +0000

    Added a QuickBuild build configuration to create a local build without running all the tests again. (RM-6297)

--- Build-Remotion.cmd
+++ Build-Remotion.cmd
@@ -10,21 +10,23 @@ echo.
 echo Choose your desired build:
 echo [1] ... Test build ^(x86-debug^)
 echo [2] ... Full build ^(x86-debug/release, x64-debug/release^)
-echo [3] ... Docs build ^(x86-debug if not present, docs^)
+echo [3] ... Quick build ^(x86-debug, no tests are run^)
+echo [4] ... Docs build ^(x86-debug if not present, docs^)
 echo           Requires Sandcastle Help File Builder to be installed!
-echo [4] ... Package ^(create zip package from pre-existent build^)
-echo [5] ... Run DependDB
-echo [6] ... Oops, nothing please - exit.
+echo [5] ... Package ^(create zip package from pre-existent build^)
+echo [6] ... Run DependDB
+echo [7] ... Oops, nothing please - exit.
 echo.
 
-choice /c:123456 /n /m "Your choice: "
+choice /c:1234567 /n /m "Your choice: "
 
 if %ERRORLEVEL%==1 goto run_test_build
 if %ERRORLEVEL%==2 goto run_full_build
-if %ERRORLEVEL%==3 goto run_docs_build
-if %ERRORLEVEL%==4 goto run_pkg_build
-if %ERRORLEVEL%==5 goto run_dependdb
-if %ERRORLEVEL%==6 goto run_exit
+if %ERRORLEVEL%==3 goto run_quick_build
+if %ERRORLEVEL%==4 goto run_docs_build
+if %ERRORLEVEL%==5 goto run_pkg_build
+if %ERRORLEVEL%==6 goto run_dependdb
+if %ERRORLEVEL%==7 goto run_exit
 goto build_succeeded
 
 :run_test_build
@@ -36,6 +38,12 @@ goto build_succeeded
 :run_full_build
 mkdir build\BuildOutput\log
 %msbuild% build\Remotion.Local.build /t:FullBuild /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
+if not %ERRORLEVEL%==0 goto build_failed
+goto build_succeeded
+
+:run_quick_build
+mkdir build\BuildOutput\log
+%msbuild% build\Remotion.Local.build /t:QuickBuild /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
 
