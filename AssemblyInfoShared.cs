68a161ec40e744fc624b70915be12968e3de4616
Michael Ketting <michael.ketting@rubicon.eu>
2016-02-23 11:04:40 +0100

    RMTP-43 Update Remotion.BuildScript to 2.0.0-alpha.12.

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -17,28 +17,11 @@
 using System;
 using System.Reflection;
 
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyConfiguration("Build type: Debug")]
-[assembly: AssemblyCompany("rubicon IT GmbH")]
-[assembly: AssemblyProduct("re-motion code generation library (typepipe.codeplex.com)")]
+[assembly: AssemblyCompany ("rubicon IT GmbH")]
+[assembly: AssemblyProduct ("re-motion code generation library (typepipe.codeplex.com)")]
 [assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU LGPL 2.1 (or later)")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("1.17.4.20000+Debug")]
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.17.4.20000")]
-[assembly: AssemblyFileVersion ("1.17.4.20000")]
+[assembly: AssemblyConfiguration ("Build type: Debug")]
+[assembly: AssemblyVersion ("2.0.0.0")]
+[assembly: AssemblyFileVersion ("2.0.0.24009")]
+[assembly: AssemblyInformationalVersion ("2.0.0-x.9+Debug")]
