a57f8158e0d732688789cb95347be5b4219315bc
Michael Ketting <michael.ketting@rubicon.eu>
2016-06-29 13:46:14 +0200

    Update metadata to version '2.0.0'.

--- AssemblyInfoShared.cs
+++ AssemblyInfoShared.cs
@@ -23,5 +23,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyConfiguration ("Build type: Debug")]
 [assembly: AssemblyVersion ("2.0.0.0")]
-[assembly: AssemblyFileVersion ("2.0.0.24009")]
-[assembly: AssemblyInformationalVersion ("2.0.0-x.9+Debug")]
+[assembly: AssemblyFileVersion ("2.0.0.30000")]
+[assembly: AssemblyInformationalVersion ("2.0.0+Debug")]
