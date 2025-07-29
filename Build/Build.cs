207e9edf453f2e4a97f38c510a8e041540ee4a28
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-07-29 14:09:19 +0200

    add enforcedLocalMachine to suported executionruntimes

--- Build/Build.cs
+++ Build/Build.cs
@@ -123,7 +123,7 @@ class Build : RemotionBuild, IDependDB, ITest
   public override void ConfigureSupportedTestDimensions (SupportedTestDimensionsBuilder supportedTestDimensions)
   {
     supportedTestDimensions.AddOperatingSystemsDimension();
-    supportedTestDimensions.AddSupportedDimension<ExecutionRuntimes>(LocalMachine, Docker_Win_NET48, Docker_Win_NET472, Docker_Win_NET462);
+    supportedTestDimensions.AddSupportedDimension<ExecutionRuntimes>(LocalMachine, EnforcedLocalMachine, Docker_Win_NET48, Docker_Win_NET472, Docker_Win_NET462);
     supportedTestDimensions.AddSupportedDimension<TargetFrameworks>(NET48, NET472, NET462);
     supportedTestDimensions.AddSupportedDimension<Configurations>(Debug, Release);
     supportedTestDimensions.AddSupportedDimension<Platforms>(x64, x86);
